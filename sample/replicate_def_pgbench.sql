8ae8df84d2f4fccd986d382cfc8d520c2baa5b0c
Yoshiharu Mori <y-mori at sraoss.co.jp>
2007-11-09 06:09:36 +0000

    ADD Sample SQL file

---
+++ sample/replicate_def_pgbench.sql
@@ -0,0 +1,18 @@
+-- $Header$
+
+INSERT INTO pgpool_catalog.replicate_def VALUES (
+    'bench_parallel',
+    'public',
+    'branches',
+    ARRAY['bid', 'bbalance', 'filler'],
+    ARRAY['integer', 'integer', 'character(88)']
+);
+
+INSERT INTO pgpool_catalog.dist_def VALUES (
+    'bench_parallel',
+    'public',
+    'tellers',
+    ARRAY['tid', 'bid', 'tbalance', 'filler'],
+    ARRAY['integer', 'integer', 'integer', 'character(84)']
+);
+
