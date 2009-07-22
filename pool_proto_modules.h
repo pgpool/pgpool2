5a8d1af8d9398c4182c5e863cd054fea7e64ec7b
Tatsuo Ishii <ishii at sraoss.co.jp>
2009-07-22 05:33:28 +0000

    Query cancel by statement timeout does not generate "kind mismatch" error any more. How to reproduce the test case:
    Directory connect to PostgreSQL.
    T1: BEGIN;
    T1: LOCK TABLE t1;
    
    T2: SET statement_timeout TO 5000;
    T2: UPDATE t1 SET ...
    
    UPDATE query aborts and error message comes out on T2:
    ERROR:  canceling statement due to statement timeout
    
    A side effect of this is, statement timeout works correctly (rather
    than statement timeout value * # of node).
    
    Note that to initiate T1 by directory connecting to slave node will not
    work (still results in kind mismatch error). But I don't think this could
    happend in the real world.
    
    Also refactor error detecting modules (detect_deadlock_error and so on).

--- pool_proto_modules.h
+++ pool_proto_modules.h
@@ -164,6 +164,7 @@ extern POOL_STATUS end_internal_transaction(POOL_CONNECTION_POOL *backend);
 extern int detect_deadlock_error(POOL_CONNECTION *master, int major);
 extern int detect_serialization_error(POOL_CONNECTION *master, int major);
 extern int detect_active_sql_transaction_error(POOL_CONNECTION *backend, int major);
+extern int detect_query_cancel_error(POOL_CONNECTION *backend, int major);
 extern bool is_partition_table(POOL_CONNECTION_POOL *backend, Node *node);
 extern POOL_STATUS pool_discard_packet(POOL_CONNECTION_POOL *cp);
 
