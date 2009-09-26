e7603c816809de8dec7f598c34668e14777b0e44
Tatsuo Ishii <ishii at sraoss.co.jp>
2009-09-26 09:17:27 +0000

    Allow to print more accurate query when Parse() detects kind mismatch errors.

--- pool_proto_modules.c
+++ pool_proto_modules.c
@@ -907,6 +907,9 @@ POOL_STATUS Parse(POOL_CONNECTION *frontend,
 	}
 	else
 	{
+		/* Save last query string for logging purpose */
+		snprintf(query_string_buffer, sizeof(query_string_buffer), "Parse: %s", stmt);
+
 		node = (Node *) lfirst(list_head(parse_tree_list));
 
 		insert_stmt_with_lock = need_insert_lock(backend, stmt, node);
