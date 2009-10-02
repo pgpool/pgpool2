0e2e892787da62db15589bcbc474c17dc57d7cc0
Tatsuo Ishii <ishii at sraoss.co.jp>
2009-10-02 07:56:42 +0000

    Fix Parse() to use wait_for_query_response() instead of synchronize().

--- pool_proto_modules.c
+++ pool_proto_modules.c
@@ -679,13 +679,12 @@ POOL_STATUS Execute(POOL_CONNECTION *frontend,
 		node = (Node *)p_stmt->query;
 		strncpy(query_string_buffer, string1, sizeof(query_string_buffer));
 
-		if ((IsA(node, PrepareStmt) || IsA(node, DeallocateStmt) ||
-			 IsA(node, VariableSetStmt) || IsA(node, DiscardStmt) ||
-			 (IsA(node, CreateStmt) && ((CreateStmt *)node)->relation->istemp)) &&
-			MASTER_SLAVE && TSTATE(backend) != 'E')
+ 		if ((IsA(node, PrepareStmt) || IsA(node, DeallocateStmt) ||
+ 			 IsA(node, VariableSetStmt)) &&
+  			MASTER_SLAVE && TSTATE(backend) != 'E')
 		{
 			/*
-			 * PREPARE, DEALLOCATE, SET, DISCARD and CREATE TEMP TABLE
+			 * PREPARE, DEALLOCATE, SET, DISCARD
 			 * should be executed on all nodes.  So we set
 			 * force_replication.
 			 */
@@ -1012,12 +1011,22 @@ POOL_STATUS Parse(POOL_CONNECTION *frontend,
 
 	if (REPLICATION || PARALLEL_MODE || MASTER_SLAVE)
 	{
-		/* We must synchronize because Parse message acquires table
+		/*
+		 * We must synchronize because Parse message acquires table
 		 * locks.
 		 */
-		pool_debug("waiting for master completing the query");
-		if (synchronize(MASTER(backend)))
+		pool_debug("Parse: waiting for master completing the query");
+		if (wait_for_query_response(frontend, MASTER(backend), string, MAJOR(backend)) != POOL_CONTINUE)
+		{
+			/* Cancel current transaction */
+			CancelPacket cancel_packet;
+
+			cancel_packet.protoVersion = htonl(PROTO_CANCEL);
+			cancel_packet.pid = MASTER_CONNECTION(backend)->pid;
+			cancel_packet.key= MASTER_CONNECTION(backend)->key;
+			cancel_request(&cancel_packet);
 			return POOL_END;
+		}
 
 		/*
 		 * We must check deadlock error because a aborted transaction
@@ -1055,9 +1064,18 @@ POOL_STATUS Parse(POOL_CONNECTION *frontend,
 			if (!VALID_BACKEND(i) || IS_MASTER_NODE_ID(i))
 				continue;
 
-			pool_debug("waiting for %dth backend completing the query", i);
-			if (synchronize(CONNECTION(backend, i)))
+			pool_debug("Parse: waiting for %dth backend completing the query", i);
+			if (wait_for_query_response(frontend, CONNECTION(backend, i), string, MAJOR(backend)) != POOL_CONTINUE)
+			{
+				/* Cancel current transaction */
+				CancelPacket cancel_packet;
+
+				cancel_packet.protoVersion = htonl(PROTO_CANCEL);
+				cancel_packet.pid = MASTER_CONNECTION(backend)->pid;
+				cancel_packet.key= MASTER_CONNECTION(backend)->key;
+				cancel_request(&cancel_packet);
 				return POOL_END;
+			}
 		}
 	}
 
