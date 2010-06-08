e277b7852157d2fc47437d0dcd3d2be7cb22d6f1
Toshihiro Kitagawa <kitagawa at sraoss.co.jp>
2010-06-08 05:39:22 +0000

    Fix pgpool.conf boolean variables to take the same range of string values as postgresql.conf.

--- pool.h
+++ pool.h
@@ -547,4 +547,7 @@ extern POOL_STATUS do_command(POOL_CONNECTION *frontend, POOL_CONNECTION *backen
 extern POOL_STATUS do_query(POOL_CONNECTION *backend, char *query, POOL_SELECT_RESULT **result, int major);
 extern void free_select_result(POOL_SELECT_RESULT *result);
 
+/* pool_config.c */
+extern int eval_logical(char *str);
+
 #endif /* POOL_H */
