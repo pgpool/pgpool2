44a431ade04ee7378de92218d29a4f816a201740
Tatsuo Ishii <ishii at sraoss.co.jp>
2010-07-27 23:24:08 +0000

    More doc updation for 3.0.

---
+++ doc/basebackup.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+# Recovery script for streaming replication.
+# This script assumes that DB node 0 is primary, and 1 is standby.
+#
+datadir=$1
+desthost=$2
+destdir=$3
+
+psql -c "SELECT pg_start_backup('Streaming Replication', true)" postgres
+
+rsync -C -a --delete -e ssh --exclude postgresql.conf --exclude postmaster.pid \
+--exclude postmaster.opts --exclude pg_log --exclude pg_xlog \
+--exclude recovery.conf $datadir/ $desthost:$destdir/
+
+ssh -T localhost mv $destdir/recovery.done $destdir/recovery.conf
+
+psql -c "SELECT pg_stop_backup()" postgres
