ff90edd8904992c4fa492415c5c61416ba25e10a
Toshihiro Kitagawa <kitagawa at sraoss.co.jp>
2010-08-17 01:36:51 +0000

    Change the dummy query of SimpleQuery to DELETE instead of INSERET, because INSERT send a command for rewriting timestamp.

--- pool_proto_modules.c
+++ pool_proto_modules.c
@@ -158,13 +158,13 @@ POOL_STATUS SimpleQuery(POOL_CONNECTION *frontend,
 		/*
 		 * Unable to parse the query. Probably syntax error or the
 		 * query is too new and our parser cannot understand. Treat as
-		 * if it were an INSERT command. Note that the INSERT command
+		 * if it were an DELETE command. Note that the DELETE command
 		 * does not execute, instead the original query will be sent
 		 * to backends, which may or may not cause an actual syntax errors.
 		 * The command will be sent to all backends in replication mode
 		 * or master/primary in master/slave mode.
 		 */
-		char *p = "INSERT INTO foo VALUES(1)";
+		char *p = "DELETE FROM foo WHERE col = 'pgpool: unable to parse the query'";
 
 		pool_log("SimpleQuery: Unable to parse the query: %s", contents);
 		parse_tree_list = raw_parser(p);
