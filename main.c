5b14ad3f7cd1c3f9efb071fb7eb86f57d9281661
Tatsuo Ishii <ishii at sraoss.co.jp>
2011-02-10 02:14:56 +0000

    Fix pool_get_node_info() so that it deals with node_number == -1 case. This could happen get_next_master_node() returns -1 because there's no master node available (all nodes down case). Also tweak trigger_failover_command() to deal with pool_get_node_info() returns NULL case. Per report from Wouter D'Haeseleer. Subject: [Pgpool-general] PGpool crashes when all DB servers go down To: "pgpool-general@pgfoundry.org" <pgpool-general@pgfoundry.org> Date: Mon, 07 Feb 2011 09:35:50 +0100

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
  * pgpool: a language independent connection pool server for PostgreSQL
  * written by Tatsuo Ishii
  *
- * Copyright (c) 2003-2010	PgPool Global Development Group
+ * Copyright (c) 2003-2011	PgPool Global Development Group
  *
  * Permission to use, copy, modify, and distribute this software and
  * its documentation for any purpose and without fee is hereby
@@ -1960,7 +1960,7 @@ static void reaper(void)
 BackendInfo *
 pool_get_node_info(int node_number)
 {
-	if (node_number >= NUM_BACKENDS)
+	if (node_number < 0 || node_number >= NUM_BACKENDS)
 		return NULL;
 
 	return &BACKEND_INFO(node_number);
@@ -2224,7 +2224,11 @@ static int trigger_failover_command(int node, const char *command_line)
 
 					case 'H': /* new master host name */
 						newmaster = pool_get_node_info(get_next_master_node());
-						string_append_char(exec_cmd, newmaster->backend_hostname);
+						if (newmaster)
+							string_append_char(exec_cmd, newmaster->backend_hostname);
+						else
+							/* no vaid new master */
+							string_append_char(exec_cmd, "");
 						break;
 
 					case 'm': /* new master node id */
