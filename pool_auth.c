c3981a5f4298230c5e01045a8e300b6899e054f1
Toshihiro Kitagawa <kitagawa at sraoss.co.jp>
2011-06-08 11:12:13 +0000

    Change "do_md5: read_password_packet failed" message into debug level. This error message is emitted if frontends(psql etc.) authenticate a password, because they might reconnect at the time.
    Reason for change:
    - PostgreSQL does not emit error messages at the time.
    - pgpool-II does not emit error messages when using the other
      authentication methods.

--- pool_auth.c
+++ pool_auth.c
@@ -857,7 +857,7 @@ static int do_md5(POOL_CONNECTION *backend, POOL_CONNECTION *frontend, int reaut
 			/* Read password packet */
 			if (read_password_packet(frontend, protoMajor, password, &size))
 			{
-				pool_error("do_md5: read_password_packet failed");
+				pool_debug("do_md5: read_password_packet failed");
 				return -1;
 			}
 
@@ -947,7 +947,7 @@ static int do_md5(POOL_CONNECTION *backend, POOL_CONNECTION *frontend, int reaut
 		/* Read password packet */
 		if (read_password_packet(frontend, protoMajor, password, &size))
 		{
-			pool_error("do_md5: read_password_packet failed");
+			pool_debug("do_md5: read_password_packet failed");
 			return -1;
 		}
 	}
