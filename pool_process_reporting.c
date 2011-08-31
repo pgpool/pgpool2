105ffd8811b75709ebf86e635c67b0c9932c0ba1
Toshihiro Kitagawa <kitagawa at sraoss.co.jp>
2011-08-31 10:27:17 +0000

    Change directive name in pool_status. flag -> backend_flag.

--- pool_process_reporting.c
+++ pool_process_reporting.c
@@ -564,9 +564,9 @@ POOL_REPORT_CONFIG* get_config(int *nrows)
 		snprintf(status[i].desc, POOLCONFIG_MAXDESCLEN, "standby delay of backend #%d", j);
 		i++;
 
-		snprintf(status[i].name, POOLCONFIG_MAXNAMELEN, "flag%d", j);
+		snprintf(status[i].name, POOLCONFIG_MAXNAMELEN, "backend_flag%d", j);
 		snprintf(status[i].value, POOLCONFIG_MAXVALLEN, "%s", pool_flag_to_str(BACKEND_INFO(j).flag));
-		snprintf(status[i].desc, POOLCONFIG_MAXDESCLEN, "backend flag #%d", j);
+		snprintf(status[i].desc, POOLCONFIG_MAXDESCLEN, "backend #%d flag", j);
 		i++;
 	}
 
