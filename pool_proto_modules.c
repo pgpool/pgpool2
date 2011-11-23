2d48fa39234b07ce827cee6bf06ff73fe8b8e541
Tatsuo Ishii <ishii@postgresql.org>
2011-11-23 17:19:54 +0900

    Fix memory leak in raw mode. This is essentially same as the fix made for 3.0.5 (commit 19a4ea9215da0b61728741fc0da2271958b09238).

--- pool_proto_modules.c
+++ pool_proto_modules.c
@@ -115,6 +115,7 @@ POOL_STATUS SimpleQuery(POOL_CONNECTION *frontend,
 
 	POOL_SESSION_CONTEXT *session_context;
 	POOL_QUERY_CONTEXT *query_context;
+	POOL_MEMORY_POOL *old_context;
 
 	/* Get session context */
 	session_context = pool_get_session_context();
@@ -147,6 +148,10 @@ POOL_STATUS SimpleQuery(POOL_CONNECTION *frontend,
 		pool_error("SimpleQuery: pool_init_query_context failed");
 		return POOL_END;
 	}
+
+	/* switch memory context */
+	old_context = pool_memory;
+	pool_memory = query_context->memory_context;
 
 	/* parse SQL string */
 	parse_tree_list = raw_parser(contents);
@@ -270,7 +275,6 @@ POOL_STATUS SimpleQuery(POOL_CONNECTION *frontend,
 					 sp->user, sp->database, remote_ps_data);
 			set_ps_display(psbuf, false);
 
-			free_parser();
 			pool_query_context_destroy(query_context);
 			pool_set_skip_reading_from_backends();
 			return POOL_CONTINUE;
@@ -335,7 +339,7 @@ POOL_STATUS SimpleQuery(POOL_CONNECTION *frontend,
 					status = insert_lock(frontend, backend, contents, (InsertStmt *)node, lock_kind);
 					if (status != POOL_CONTINUE)
 					{
-						free_parser();
+						pool_query_context_destroy(query_context);
 						return status;
 					}
 				}
@@ -343,7 +347,7 @@ POOL_STATUS SimpleQuery(POOL_CONNECTION *frontend,
 		}
 		else if (REPLICATION && contents == NULL && start_internal_transaction(frontend, backend, node))
 		{
-			free_parser();
+			pool_query_context_destroy(query_context);
 			return POOL_ERROR;
 		}
 	}
@@ -438,16 +442,14 @@ POOL_STATUS SimpleQuery(POOL_CONNECTION *frontend,
 			{
 				/* Send query to all DB nodes at once */
 				status = pool_send_and_wait(query_context, 0, 0);
-				/*
-				free_parser();
-				*/
+				/* free_parser(); */
 				return status;
 			}
 
 			/* Send the query to master node */
 			if (pool_send_and_wait(query_context, 1, MASTER_NODE_ID) != POOL_CONTINUE)
 			{
-				free_parser();
+				pool_query_context_destroy(query_context);
 				return POOL_END;
 			}
 		}
@@ -457,7 +459,7 @@ POOL_STATUS SimpleQuery(POOL_CONNECTION *frontend,
 		 */
 		if (pool_send_and_wait(query_context, -1, MASTER_NODE_ID) != POOL_CONTINUE)
 		{
-			free_parser();
+			pool_query_context_destroy(query_context);
 			return POOL_END;
 		}
 
@@ -466,25 +468,25 @@ POOL_STATUS SimpleQuery(POOL_CONNECTION *frontend,
 		{
 			if (pool_send_and_wait(query_context, 1, MASTER_NODE_ID) != POOL_CONTINUE)
 			{
-/*
-				free_parser();
-*/
+				pool_query_context_destroy(query_context);
 				return POOL_END;
 			}
 		}
-		free_parser();
+		/* free_parser(); */
 	}
 	else
 	{
 		if (pool_send_and_wait(query_context, 1, MASTER_NODE_ID) != POOL_CONTINUE)
 		{
-			free_parser();
+			pool_query_context_destroy(query_context);
 			return POOL_END;
 		}
-/*
-		free_parser();
-*/
+		/* free_parser(); */
 	}
+
+	/* switch memory context */
+	pool_memory = old_context;
+
 	return POOL_CONTINUE;
 }
 
@@ -785,13 +787,13 @@ POOL_STATUS Parse(POOL_CONNECTION *frontend, POOL_CONNECTION_POOL *backend,
 			kind = pool_read_kind(backend);
 			if (kind != 'Z')
 			{
-				/* free_parser(); */
+				pool_query_context_destroy(query_context);
 				return POOL_END;
 			}
 
 			if (ReadyForQuery(frontend, backend, 0) != POOL_CONTINUE)
 			{
-				/* free_parser(); */
+				pool_query_context_destroy(query_context);
 				return POOL_END;
 			}
 
@@ -812,7 +814,7 @@ POOL_STATUS Parse(POOL_CONNECTION *frontend, POOL_CONNECTION_POOL *backend,
 			status = insert_lock(frontend, backend, stmt, (InsertStmt *)query_context->parse_tree, insert_stmt_with_lock);
 			if (status != POOL_CONTINUE)
 			{
-				/* free_parser(); */
+				pool_query_context_destroy(query_context);
 				return status;
 			}
 		}
@@ -832,7 +834,7 @@ POOL_STATUS Parse(POOL_CONNECTION *frontend, POOL_CONNECTION_POOL *backend,
 		pool_debug("Parse: waiting for master completing the query");
 		if (pool_extended_send_and_wait(query_context, "P", len, contents, 1, MASTER_NODE_ID) != POOL_CONTINUE)
 		{
-			/* free_parser(); */
+			pool_query_context_destroy(query_context);
 			return POOL_END;
 		}
 
@@ -845,7 +847,7 @@ POOL_STATUS Parse(POOL_CONNECTION *frontend, POOL_CONNECTION_POOL *backend,
 		deadlock_detected = detect_deadlock_error(MASTER(backend), MAJOR(backend));
 		if (deadlock_detected < 0)
 		{
-			/* free_parser(); */
+			pool_query_context_destroy(query_context);
 			return POOL_END;
 		}
 		else
@@ -871,7 +873,7 @@ POOL_STATUS Parse(POOL_CONNECTION *frontend, POOL_CONNECTION_POOL *backend,
 
 			if (pool_send_and_wait(error_qc, -1, MASTER_NODE_ID) != POOL_CONTINUE)
 			{
-				/* free_parser(); */
+				pool_query_context_destroy(query_context);
 				return POOL_END;
 			}
 
@@ -883,7 +885,7 @@ POOL_STATUS Parse(POOL_CONNECTION *frontend, POOL_CONNECTION_POOL *backend,
 		{
 			if (pool_extended_send_and_wait(query_context, "P", len, contents, -1, MASTER_NODE_ID) != POOL_CONTINUE)
 			{
-				/* free_parser(); */
+				pool_query_context_destroy(query_context);
 				return POOL_END;
 			}
 		}
@@ -892,7 +894,7 @@ POOL_STATUS Parse(POOL_CONNECTION *frontend, POOL_CONNECTION_POOL *backend,
 	{
 		if (pool_extended_send_and_wait(query_context, "P", len, contents, 1, MASTER_NODE_ID) != POOL_CONTINUE)
 		{
-			/* free_parser(); */
+			pool_query_context_destroy(query_context);
 			return POOL_END;
 		}
 	}
