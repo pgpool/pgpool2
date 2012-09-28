6189733f3dc921b66fc98a79bc7e994d8674bd54
Tatsuo Ishii <ishii@postgresql.org>
2012-09-28 11:47:45 +0900

    Fix possible memory leak in nodeToString(). Since the memory context could be session context, memory used for String object may be considerable if user session continues for hours or days. See bug track #24 for more details.

--- parser/outfuncs.c
+++ parser/outfuncs.c
@@ -5536,8 +5536,12 @@ char *
 nodeToString(void *obj)
 {
 	String *str;
+	char *ret;
 
 	str = init_string("");
 	_outNode(str, obj);
-	return str->data;
+
+	ret = str->data;
+	pfree(str);
+	return ret;
 }
