26ed7692523b80139847accd56cc414e92eafdcd
Tatsuo Ishii <ishii@postgresql.org>
2013-06-30 17:25:45 +0900

    Add regression test for bug#63.

---
+++ test/regression/tests/056.bug63/test.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+#-------------------------------------------------------------------
+# test script for bug#63
+# On memory query cache occasionally segfaults.
+#
+# Fixed in: http://git.postgresql.org/gitweb/?p=pgpool2.git;a=commit;h=d428c5c27600dd82161dda6a240737d88db78732
+# requires Java PostgreSQL JDBC driver.
+JDBC_DRIVER=/usr/local/pgsql/share/postgresql-9.2-1002.jdbc4.jar
+WHOAMI=`whoami`
+source $TESTLIBS
+TESTDIR=testdir
+
+rm -fr $TESTDIR
+mkdir $TESTDIR
+cd $TESTDIR
+
+# create test environment
+echo -n "creating test environment..."
+sh $PGPOOL_SETUP -m s -n 1 || exit 1
+echo "done."
+
+source ./bashrc.ports
+
+export PGPORT=$PGPOOL_PORT
+
+echo "memory_cache_enabled = on" >> etc/pgpool.conf
+echo "jdbc.url=jdbc:postgresql://localhost:$PGPOOL_PORT/test" > jdbctest.prop
+echo "jdbc.user=$WHOAMI" >> jdbctest.prop
+echo "jdbc.password=" >> jdbctest.prop
+cp ../jdbctest2.java .
+javac jdbctest2.java
+export CLASSPATH=.:$JDBC_DRIVER
+./startall
+wait_for_pgpool_startup
+
+pgbench -i test
+java jdbctest2
+
+if [ $? != 0 ];then
+	./shutdownall
+	exit 1
+fi
+
+./shutdownall
+exit 0
