pgpool/pgpool2

commit 8b803e1ea38c3ac9a7775f7fb9ba43870bca7dca

Add wait_for_pgpool_reload function
This avoids pgpool recovery before new backends information reloaded.

test/pgpool_setup
@@ -26,6 +26,7 @@
 # The user run this script will become the PostgreSQL super user as
 # well.  Current directory must be empty.  Assume that appropreate
 # pgpool and PostgreSQL binaries are in the command search path.
+# The user must have passwordless access to localhost over SSH.
 #
 # Rayout after executing this script:
 #  data[0-]: PostgreSQL database clusters
@@ -232,7 +233,7 @@ DEST=$1
 DESTDIR=$2
 PGCTL=__PGBIN__/pg_ctl
 
-ssh -T $DEST $PGCTL -w -D $DESTDIR start 2>/dev/null 1>/dev/null < /dev/null &
+ssh -T $PGCTL -w -D $DESTDIR start 2>/dev/null 1>/dev/null < /dev/null &
 EOF
 
 #-------------------------------------------
@@ -559,6 +560,23 @@ function wait_for_pgpool_startup {
 	done
 }
 
+#-------------------------------------------
+# wait for pgpool reload finished
+#-------------------------------------------
+function wait_for_pgpool_reload {
+	timeout=20
+	num_node=$1
+
+	while [ $timeout -gt  0 ]
+	do
+		N=`$PSQL -p $PGPOOL_PORT -c "show pool_status" test | grep backend_data | wc -l`
+		if [ $N = $num_node ];then
+			break;
+		fi
+		timeout=`expr $timeout - 1`
+	done
+}
+
 #################################################################################
 #
 # main script
@@ -773,6 +791,7 @@ EOF
 		pgpool -f $BASEDIR/etc/pgpool.conf reload
 	fi
 
+	wait_for_pgpool_reload $NUMCLUSTERS
 $PSQL -p $PGPOOL_PORT -c "show pool_nodes" test
 
 # recovery data1 and so on

commit fc65ab869859384bee074526c944b577abb0dd33

Fix bug introduced in 8b803e1ea38c3ac9a7775f7fb9ba43870bca7dca (Add wait_for_pgpool_reload function). Add -p option to specify start port #.

test/pgpool_setup
@@ -16,12 +16,16 @@
 # directory for *testing* purpose.
 # Do not use this tool for production environment!
 #
-# usage: pgpool_setup [-m r|s][-n num_clusters][--no-stop]
+# usage: pgpool_setup [-m r|s][-n num_clusters][-p base_port][--no-stop]
 # -m s: create an installation as streaming replication mode.
 # (the default)
 # -m r: create an installation as native replication mode.
-#-n num_clusters: create num_clusters PostgreSQL database cluster nodes
-#--no-stop: do not stop pgpool and PostgreSQL after the work
+# -n num_clusters: create num_clusters PostgreSQL database cluster nodes
+# -p base_port: specify base port. The first PostgreSQL node's port is
+# -base_port. Second PostgreSQL node's port is base_port+1... nth
+#  PostgreSQL node's port is base_port+n-1, pgpool port is
+#  base_port+n, pcp port is base_port+n+1.
+# --no-stop: do not stop pgpool and PostgreSQL after the work
 #
 # The user run this script will become the PostgreSQL super user as
 # well.  Current directory must be empty.  Assume that appropreate
@@ -233,7 +237,7 @@ DEST=$1
 DESTDIR=$2
 PGCTL=__PGBIN__/pg_ctl
 
-ssh -T $PGCTL -w -D $DESTDIR start 2>/dev/null 1>/dev/null < /dev/null &
+ssh -T $DEST $PGCTL -w -D $DESTDIR start 2>/dev/null 1>/dev/null < /dev/null &
 EOF
 
 #-------------------------------------------
@@ -584,12 +588,12 @@ function wait_for_pgpool_reload {
 ################################################################################
 function usage()
 {
-	echo "usage: $0 [-m r|s] [-n num_clusters] [--no-stop] ";exit 1
+	echo "usage: $0 [-m r|s] [-n num_clusters] [-p base_port] [--no-stop] ";exit 1
 }
 
 #-------------------------------------------
 # Argument check
-# usage: $0 [-m r|s] [-n num_clusters] [--no-stop]"
+# usage: $0 [-m r|s] [-n num_clusters] [-p base_port] [--no-stop]"
 #-------------------------------------------
 #
 # default mode is streaming replication mode
@@ -608,6 +612,9 @@ do
 	elif [ $1 = "-n" ];then
 		shift
 		NUMCLUSTERS=$1
+	elif [ $1 = "-p" ];then
+		shift
+		BASEPORT=$1
 	elif [ $1 = "--no-stop" ];then
 		shift
 		NO_STOP="true"