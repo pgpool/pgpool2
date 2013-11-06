e93a0d35a36369117da3a58440f3c883e45f0f46
Tatsuo Ishii <ishii@postgresql.org>
2013-11-06 20:10:24 +0900

    Fix target node selection logic when "DEALLOCATE portal|statement".
    When "DEALLOCATE portal|statement" is used and last prepared statement
    or portal was not found, target node selection map is not
    set. Probably this is not actually harmful because prepared statement
    or portal was not found is an error case. The bug was there since day
    0.
    
    Per Coverity report "1111491 Structurally dead code".

--- pool_query_context.c
+++ pool_query_context.c
@@ -1241,11 +1241,10 @@ void where_to_send_deallocate(POOL_QUERY_CONTEXT *query_context, Node *node)
 	DeallocateStmt *d = (DeallocateStmt *)node;
 	POOL_SENT_MESSAGE *msg;
 
-	/* DELLOCATE ALL? */
+	/* DEALLOCATE ALL? */
 	if (d->name == NULL)
 	{
 		pool_setall_node_to_be_sent(query_context);
-		return;
 	}
 	else
 	{
@@ -1257,11 +1256,11 @@ void where_to_send_deallocate(POOL_QUERY_CONTEXT *query_context, Node *node)
 			/* Inherit same map from PREPARE or PARSE */
 			pool_copy_prep_where(msg->query_context->where_to_send,
 								 query_context->where_to_send);
+			return;
 		}
-		return;
+		/* prepared statement was not found */
+		pool_setall_node_to_be_sent(query_context);
 	}
-	/* prepared statement was not found */
-	pool_setall_node_to_be_sent(query_context);
 }
 
 /*
