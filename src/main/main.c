679553699f7f8e5ba8d3e39d8000d2e6ca753d22
Tatsuo Ishii <ishii@postgresql.org>
2014-01-15 08:14:30 +0900

    Fix primary node detection logic.
    There's a possibility that primary node is not detected. This happens
    in following situation.  node 0: primary, node 1: standby. Node 0 goes
    down. Health checking detects the fact but local status is not updated
    yet. Primary node finding (find_primary_node) runs. Node 0's status is
    yet healthy. Because find_primary_node fails to connect to node 0, it
    immediately returns -1 and fails to find that fact that node 1 is now
    primary.
    
    Fix is just continuing to look for primary node when fails to connect
    to a node.
    
    Per [pgpool-general: 2409].

--- src/main/main.c
+++ src/main/main.c
@@ -2841,7 +2841,14 @@ static int find_primary_node(void)
 		if (!s)
 		{
 			pool_error("find_primary_node: make_persistent_connection failed");
-			return -1;
+
+			/*
+			 * It is possible that a node is down even if
+			 * VALID_BACKEND tells it's valid.  This could happen
+			 * before health checking detects the failure.
+			 * Thus we should continue to look for primary node.
+			 */
+			continue;
 		}
 		con = s->con;
 		status = do_query(con, "SELECT pg_is_in_recovery()",
