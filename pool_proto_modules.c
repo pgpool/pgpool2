85a423f9f59089fa6c05b6267460e4d9be030912
Tatsuo Ishii <ishii@postgresql.org>
2014-07-16 18:22:21 +0900

    Fix wrong error message.

--- pool_proto_modules.c
+++ pool_proto_modules.c
@@ -2546,7 +2546,7 @@ POOL_STATUS ProcessFrontendResponse(POOL_CONNECTION *frontend,
 	session_context = pool_get_session_context();
 	if (!session_context)
 	{
-		pool_error("Parse: cannot get session context");
+		pool_error("ProcessFrontendResponse: cannot get session context");
 		return POOL_END;
 	}
 
