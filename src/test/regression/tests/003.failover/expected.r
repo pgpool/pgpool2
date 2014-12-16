d208c8b3e8a7ac329af4227854c8c4c9c42d9f0b
Yugo Nagata <nagata@sraoss.co.jp>
2014-12-16 22:06:09 +0900

    Fix failover test to support select_cnt feature of show pool_nodes

--- src/test/regression/tests/003.failover/expected.r
+++ src/test/regression/tests/003.failover/expected.r
@@ -1,6 +1,6 @@
- node_id | hostname | port  | status | lb_weight |  role  
----------+----------+-------+--------+-----------+--------
- 0       | /tmp     | 11002 | 2      | 0.500000  | master
- 1       | /tmp     | 11003 | 3      | 0.500000  | slave
+ node_id | hostname | port  | status | lb_weight |  role  | select_cnt 
+---------+----------+-------+--------+-----------+--------+------------
+ 0       | /tmp     | 11002 | 2      | 0.500000  | master | 0
+ 1       | /tmp     | 11003 | 3      | 0.500000  | slave  | 0
 (2 rows)
 
--- src/test/regression/tests/003.failover/expected.s
+++ src/test/regression/tests/003.failover/expected.s
@@ -1,6 +1,6 @@
- node_id | hostname | port  | status | lb_weight |  role   
----------+----------+-------+--------+-----------+---------
- 0       | /tmp     | 11002 | 2      | 0.500000  | primary
- 1       | /tmp     | 11003 | 3      | 0.500000  | standby
+ node_id | hostname | port  | status | lb_weight |  role   | select_cnt 
+---------+----------+-------+--------+-----------+---------+------------
+ 0       | /tmp     | 11002 | 2      | 0.500000  | primary | 0
+ 1       | /tmp     | 11003 | 3      | 0.500000  | standby | 0
 (2 rows)
 
