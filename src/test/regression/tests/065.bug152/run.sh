21a715f64a93bc29dd0d2b11fdbb75aea7fc7819
Tatsuo Ishii <ishii@postgresql.org>
2015-10-29 13:23:17 +0900

    Fix regression test 065.
    The path to JDBC driver was explicitly defined. This is wrong. This
    should be inherited from JDBC_DRIVER environment variable.

--- src/test/regression/tests/065.bug152/run.sh
+++ src/test/regression/tests/065.bug152/run.sh
@@ -1,5 +1,3 @@
-#JDBC_DRIVER=/usr/local/pgsql/share/postgresql-9.4-1204.jdbc41.jar
-JDBC_DRIVER=/usr/local/pgsql/share/postgresql-9.2-1003.jdbc4.jar
 export CLASSPATH=.:$JDBC_DRIVER
 javac Main.java
 java Main
