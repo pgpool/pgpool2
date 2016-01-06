70ab80ed641c506048dce5c5af654cc8a9d0613f
Yugo Nagata <nagata@sraoss.co.jp>
2016-01-06 13:26:46 +0900

    Fix port numbers in expected result of show pool_nodes

--- test/regression/tests/003.failover/create_expected.sql
+++ test/regression/tests/003.failover/create_expected.sql
@@ -8,10 +8,10 @@ CREATE TEMP TABLE tmp (
   mode text);
 
 INSERT INTO tmp VALUES
-('0',:dir,'11002','2','0.500000','primary','s'),
-('1',:dir,'11003','3','0.500000','standby','s'),
-('0',:dir,'11002','2','0.500000','master','r'),
-('1',:dir,'11003','3','0.500000','slave','r');
+('0',:dir,'11000','2','0.500000','primary','s'),
+('1',:dir,'11001','3','0.500000','standby','s'),
+('0',:dir,'11000','2','0.500000','master','r'),
+('1',:dir,'11001','3','0.500000','slave','r');
 
 SELECT node_id,hostname,port,status,lb_weight,role
 FROM tmp
