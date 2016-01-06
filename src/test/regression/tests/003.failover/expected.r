73cad019d535681c1e674a311a0e65af6d925d2d
Yugo Nagata <nagata@sraoss.co.jp>
2016-01-06 13:01:41 +0900

    Fix regression test 003.failover for rhel7 postgresql rpm
    The expected result of 'show pool_nodes' is fixed to use
    PGSOCKET_DIR in hostname column.

--- src/test/regression/tests/003.failover/expected.r
+++
@@ -1,6 +0,0 @@
- node_id | hostname | port  | status | lb_weight |  role  | select_cnt 
----------+----------+-------+--------+-----------+--------+------------
- 0       | /tmp     | 11002 | 2      | 0.500000  | master | 0
- 1       | /tmp     | 11003 | 3      | 0.500000  | slave  | 0
-(2 rows)
-
