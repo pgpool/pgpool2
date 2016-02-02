96169e750d2e1b02ca843179a223e3f56c8a1dce
Yugo Nagata <nagata@sraoss.co.jp>
2016-02-02 10:36:06 +0900

    Allow timeout value to be specified by the command option

--- src/test/regression/regress.sh
+++ src/test/regression/regress.sh
@@ -110,6 +110,7 @@ function print_usage
 	printf "  -m   install/noinstall   make install pgpool to temp directory for executing regression tests [Default: install]\n" >&2
 	printf "  -j   FILE                Postgres jdbc jar file path\n" >&2
 	printf "  -s   DIRECTORY           unix socket directory\n" >&2
+	printf "  -t   TIMEOUT             timeout value for each test (sec)\n" >&2
 	printf "  -?                       print this help and then exit\n\n" >&2
 	printf "Please read the README for details on adding new tests\n" >&2
 
@@ -117,7 +118,7 @@ function print_usage
 
 trap "echo ; exit 0" SIGINT SIGQUIT
 
-while getopts "p:m:i:j:b:s:?" OPTION
+while getopts "p:m:i:j:b:s:t:?" OPTION
 do
   case $OPTION in
     p)  PG_INSTALL_DIR="$OPTARG";;
@@ -126,6 +127,7 @@ do
     j)  JDBC_DRIVER="$OPTARG";;
     b)  PGBENCH_PATH="$OPTARG";;
     s)  PGSOCKET_DIR="$OPTARG";;
+    t)  TIMEOUT="$OPTARG";;
     ?)  print_usage
         exit 2;;
   esac
