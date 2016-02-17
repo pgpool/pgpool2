4f41a591ffa67ae6601d35ea8921d2f513e9a6fd
Yugo Nagata <nagata@sraoss.co.jp>
2016-02-17 17:24:54 +0900

    Fix previous bronken commit
    The following commit was broken and regress test 001 failed.
    - Fix memorry leak reported by Coverity (CID 1350095)

--- src/utils/pool_select_walker.c
+++ src/utils/pool_select_walker.c
@@ -206,6 +206,7 @@ int pattern_compare(char *str, const int type, const char *param_name)
 						errdetail("pattern_compare: %s (%s) matched: %s",
 							   param_name, lists_patterns[i].pattern, s)));
 				result = 1;
+				break;
 			/* return 1 if string matches blacklist pattern */
 			case BLACKLIST:
 				ereport(DEBUG2,
@@ -213,10 +214,12 @@ int pattern_compare(char *str, const int type, const char *param_name)
 						 errdetail("pattern_compare: %s (%s) matched: %s",
 								   param_name, lists_patterns[i].pattern, s)));
 				result = 1;
+				break;
 			default:
 				ereport(WARNING,
 						(errmsg("pattern_compare: \"%s\" unknown pattern match type: \"%s\"", param_name, s)));
 				result = -1;
+				break;
 			}
 			/* return the result */
 			break;
