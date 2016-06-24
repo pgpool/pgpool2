320fc2b18518a2ee3426e09d2b0c84640c0569db
Tatsuo Ishii <ishii@postgresql.org>
2016-06-24 17:31:47 +0900

    Fix buffer over run problem in "show pool_nodes".
    While processing "show pool_nodes", the buffer for hostname was too
    short. It should be same size as the buffer used for pgpool.conf.
    Problem reported by a twitter user who is using pgpool on AWS (which
    could have very long hostname).

--- pcp/libpcp_ext.h
+++ pcp/libpcp_ext.h
@@ -185,8 +185,8 @@ typedef struct {
 
 /* nodes report struct */
 typedef struct {
-        char node_id[POOLCONFIG_MAXSTATLEN+1];
-	char hostname[POOLCONFIG_MAXIDENTLEN+1];
+	char node_id[POOLCONFIG_MAXSTATLEN+1];
+	char hostname[MAX_DB_HOST_NAMELEN+1];
 	char port[POOLCONFIG_MAXIDENTLEN+1];
 	char status[POOLCONFIG_MAXSTATLEN+1];
 	char lb_weight[POOLCONFIG_MAXWEIGHTLEN+1];
