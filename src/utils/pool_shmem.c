460a6144573da9a5614f7f45be21561ddf0801fa
Tatsuo Ishii <ishii@postgresql.org>
2017-02-23 16:05:11 +0900

    Fix to release shared memory segments when Pgpool-II exits.
    Per bug272. From the bug report.
    "This cause the creation of a lot of segments if you start and stop
    pgpool continuously (and in a testing fase it could be normal). Lot of
    segments bring to reach the shmem OS configuration limit and than
    suddenly stops (pgpool) working."

--- src/utils/pool_shmem.c
+++ src/utils/pool_shmem.c
@@ -104,8 +104,10 @@ IpcMemoryDelete(int status, Datum shmId)
   	if (shmctl(shmId, IPC_STAT, &shmStat) < 0
   		&& (errno == EINVAL || errno == EACCES))
   		return;
+#ifdef NOT_USED
   	else if (shmStat.shm_nattch != 0)
   		return;
+#endif
 
 	if (shmctl(shmId, IPC_RMID, NULL) < 0)
 		ereport(LOG,
