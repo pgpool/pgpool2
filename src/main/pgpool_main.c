pgpool/pgpool2

commit e4ce880bd36b8f249bf693c086a1313148f3449a

Fix corner case bug in Pgpool-II starting up.
It is possible that a failover request is accepted before primary node
is searched.  This leads Pgpool-II to a strange state: there's no
primary node if the failed node was a primary node (even if new
primary node exists as a result of promotion of existing standby).

See [pgpool-hackers: 2321] for more details.

src/main/pgpool_main.c
@@ -193,6 +193,8 @@ int PgpoolMain(bool discard_status, bool clear_memcache_oidmaps)
 	MemoryContext MainLoopMemoryContext;
 	sigjmp_buf	local_sigjmp_buf;
 
+	bool first = true;
+
 	/* For PostmasterRandom */
 	gettimeofday(&random_start_time, NULL);
 
@@ -310,6 +312,15 @@ int PgpoolMain(bool discard_status, bool clear_memcache_oidmaps)
 											  ALLOCSET_DEFAULT_INITSIZE,
 											  ALLOCSET_DEFAULT_MAXSIZE);
 
+	/*
+	 * if the primary node id is not loaded by watchdog, search for it
+	 */
+	if (Req_info->primary_node_id < 0)
+	{
+		/* Save primary node id */
+		Req_info->primary_node_id = find_primary_node_repeatedly();
+	}
+
 	/* fork a child for PCP handling */
 	pcp_unix_fd = create_unix_domain_socket(pcp_un_addr);
 	/* Add onproc exit to clean up the unix domain socket at exit */
@@ -326,17 +337,6 @@ int PgpoolMain(bool discard_status, bool clear_memcache_oidmaps)
 
 	retrycnt = 0;		/* reset health check retry counter */
 	sys_retrycnt = 0;	/* reset SystemDB health check retry counter */
-
-	/*
-	 * check for child signals to ensure child startup before reporting successfull start
-	 */
-	CHECK_REQUEST;
-
-	ereport(LOG,
-			(errmsg("%s successfully started. version %s (%s)", PACKAGE, VERSION, PGPOOLVERSION)));
-
-	/* Save primary node id */
-	Req_info->primary_node_id = find_primary_node();
 
 	if (sigsetjmp(local_sigjmp_buf, 1) != 0)
 	{
@@ -417,6 +417,15 @@ int PgpoolMain(bool discard_status, bool clear_memcache_oidmaps)
 		bool all_nodes_healthy;
 		CHECK_REQUEST;
 
+		/*
+		 * check for child signals to ensure child startup before reporting
+		 * successfull start.
+		 */
+		if (first)
+			ereport(LOG,
+					(errmsg("%s successfully started. version %s (%s)", PACKAGE, VERSION, PGPOOLVERSION)));
+		first = false;
+
 		/* do we need health checking for PostgreSQL? */
 		if (pool_config->health_check_period > 0)
 		{
@@ -479,7 +488,7 @@ int PgpoolMain(bool discard_status, bool clear_memcache_oidmaps)
 			}
 			pool_sleep(pool_config->health_check_period);
 		}
-		else /* Health Check is not enable and we have not much to do */
+		else /* Health Check is not enabled and we have not much to do */
 		{
 			processState = SLEEPING;
 			for (;;)

src/test/regression/tests/003.failover/create_expected_node0.sql
@@ -1,0 +1,18 @@
+CREATE TEMP TABLE tmp (
+  node_id text,
+  hostname text,
+  port text,
+  status text,
+  lb_weight text,
+  role text,
+  mode text);
+
+INSERT INTO tmp VALUES
+('0',:dir,'11002','3','0.500000','standby','s'),
+('1',:dir,'11003','2','0.500000','primary','s'),
+('0',:dir,'11002','3','0.500000','slave','r'),
+('1',:dir,'11003','2','0.500000','master','r');
+
+SELECT node_id,hostname,port,status,lb_weight,role
+FROM tmp
+WHERE mode = :mode

src/test/regression/tests/003.failover/test.sh
@@ -24,7 +24,7 @@ do
 
 	$PSQL -c "show pool_nodes" test
 
-	# trrigger failover
+	# trigger failover
 	$PG_CTL -D data1 -m f stop
 	wait_for_pgpool_startup
 	$PSQL -c "show pool_nodes" test > result
@@ -37,6 +37,22 @@ do
 		exit 1
 	fi
 		
+	./shutdownall
+
+	./startall
+#	wait_for_pgpool_startup
+	# trigger failover on node 0
+	$PG_CTL -D data0 -m f stop
+	$PSQL -c "show pool_nodes" test |sed -e 's/true /false/'> result
+
+	# check the output of "show pool_nodes".
+	LANG=C $PSQL -f ../create_expected_node0.sql -v mode="'$mode'" -v dir="'$PGSOCKET_DIR'" test | tail -n 6 > expected
+	cmp result expected > /dev/null 2>&1
+	if [ $? != 0 ];then
+		./shutdownall
+		exit 1
+	fi
+
 	./shutdownall
 
 	cd ..

src/test/regression/tests/055.backend_all_down/test.sh
@@ -17,6 +17,8 @@ echo -n "creating test environment..."
 $PGPOOL_SETUP -m s -n 2 || exit 1
 echo "done."
 
+echo "search_primary_node_timeout = 5" >> etc/pgpool.conf
+
 source ./bashrc.ports
 
 export PGPORT=$PGPOOL_PORT

commit f484c6138304172f7d4ef4d02f1b15fad5b3cbf8

Fix finding primary node is not working.
It was reported that finding primary node was not executed in local
mailing list [pgpool-general-jp: 1462]. The bug was introduced in
commit e4ce880bd36b8f249bf693c086a1313148f3449a between 3.4.11 and
3.4.12.  This requires the Req_info->primary_node_id to negatives
value but I forgot to miss the part. Fix the problem by initializing
Req_info->primary_node_id.

Note that the bug was not in 3.5 or above because commit
bda946e718fe6f3605eb7e82ada8754bd84a279c has made it. Unfortunately I
missed the commit was not in 3.4 stable.

src/main/pgpool_main.c
@@ -2828,6 +2828,7 @@ static void initialize_shared_mem_objects(bool clear_memcache_oidmaps)
 	Req_info->conn_counter = 0;
 	Req_info->switching = false;
 	Req_info->request_queue_head = Req_info->request_queue_tail = -1;
+	Req_info->primary_node_id = -2;
 	InRecovery = pool_shared_memory_create(sizeof(int));
 	*InRecovery = RECOVERY_INIT;
 