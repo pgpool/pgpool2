719fb0639cc6805f2b4365977a983e1173ed0a44
Tatsuo Ishii <ishii@postgresql.org>
2017-12-10 21:04:15 +0900

    Fix returning transaction state when "ready for query" message received.
    We return primary or master node state of ready for query message to
    frontend. In most cases this is good. However if other than primary
    node or master node returns an error state (this could happen if load
    balance node is other than primary or master node and the query is an
    errornous SELECT), this should be returned to frontend, because the
    frontend already received an error.
    
    How much this affects to applications is not clear. However this
    behavior has been there for long time, probably applications do not
    care the state returned by ready for query message.

--- src/protocol/pool_proto_modules.c
+++ src/protocol/pool_proto_modules.c
@@ -1563,6 +1563,7 @@ POOL_STATUS ReadyForQuery(POOL_CONNECTION *frontend,
 	POOL_SESSION_CONTEXT *session_context;
 	Node *node = NULL;
 	char *query = NULL;
+	bool got_estate = false;
 
 	/*
 	 * It is possible that the "ignore until sync is received" flag was set if
@@ -1733,6 +1734,13 @@ POOL_STATUS ReadyForQuery(POOL_CONNECTION *frontend,
 			{
 				state = kind;
 			}
+			/*
+			 * However, if the state is 'E', then frontend should had been
+			 * already reported an ERROR. So, to match with that, let be the
+			 * state to be returned to frontend.
+			 */
+			if (kind == 'E')
+				got_estate = true;
 		}
 	}
 
@@ -1744,6 +1752,8 @@ POOL_STATUS ReadyForQuery(POOL_CONNECTION *frontend,
 		{
 			len = htonl(len);
 			pool_write(frontend, &len, sizeof(len));
+			if (got_estate)
+				state = 'E';
 			pool_write(frontend, &state, 1);
 		}
 		pool_flush(frontend);
