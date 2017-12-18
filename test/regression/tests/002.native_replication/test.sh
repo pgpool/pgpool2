91723e73b59181bd7f2389b81192e1881ae9388a
Bo Peng <pengbo@sraoss.co.jp>
2017-12-18 11:51:53 +0900

    Fix timestamp data inconsistency by replication mode.
    From PostgreSQL10 the column default value such as 'CURRENT_DATE' changes,
    Pgpool-II should also rewrite timestamp by the added default values in stead of
    "'now'::text::date".

--- test/regression/tests/002.native_replication/test.sh
+++ test/regression/tests/002.native_replication/test.sh
@@ -26,10 +26,18 @@ wait_for_pgpool_startup
 
 $PSQL test <<EOF
 CREATE TABLE t1(i SERIAL, j TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP);
+CREATE TABLE t2(i SERIAL, j DATE DEFAULT CURRENT_DATE);
+CREATE TABLE t3(i SERIAL, j TIME WITH TIME ZONE DEFAULT CURRENT_TIME);
+CREATE TABLE t4(i SERIAL, j TIME WITH TIME ZONE DEFAULT LOCALTIME);
+CREATE TABLE t5(i SERIAL, j TIMESTAMP WITH TIME ZONE DEFAULT LOCALTIMESTAMP);
 EOF
 
 cat > pgbench.sql <<EOF
 INSERT INTO t1 VALUES (DEFAULT);
+INSERT INTO t2 VALUES (DEFAULT);
+INSERT INTO t3 VALUES (DEFAULT);
+INSERT INTO t4 VALUES (DEFAULT);
+INSERT INTO t5 VALUES (DEFAULT);
 EOF
 
 $PGBENCH -i test
@@ -40,9 +48,9 @@ $PGBENCH -f pgbench.sql -c 10 -t 10 test
 cp ../PgTester.java .
 javac PgTester.java
 export CLASSPATH=.:$JDBC_DRIVER
-psql -f ../create.sql test
+$PSQL -f ../create.sql test
 env
-psql -f $PGPOOL_INSTALL_DIR/share/pgpool-II/insert_lock.sql test
+$PSQL -f $PGPOOL_INSTALL_DIR/share/pgpool-II/insert_lock.sql test
 
 java PgTester 0 &
 java PgTester 10 &
@@ -51,11 +59,19 @@ java PgTester 1000 &
 wait
 
 $PSQL -p 11001 test <<EOF
-\copy (SELECT * FROM t1 ORDER BY i) to 'dump_t0.txt'
+\copy (SELECT * FROM t1 ORDER BY i) to 'dump_t1_n0.txt'
+\copy (SELECT * FROM t2 ORDER BY i) to 'dump_t2_n0.txt'
+\copy (SELECT * FROM t3 ORDER BY i) to 'dump_t3_n0.txt'
+\copy (SELECT * FROM t4 ORDER BY i) to 'dump_t4_n0.txt'
+\copy (SELECT * FROM t5 ORDER BY i) to 'dump_t5_n0.txt'
 EOF
 
 $PSQL -p 11001 test <<EOF
-\copy (SELECT * FROM t1 ORDER BY i) to 'dump_t1.txt'
+\copy (SELECT * FROM t1 ORDER BY i) to 'dump_t1_n1.txt'
+\copy (SELECT * FROM t2 ORDER BY i) to 'dump_t2_n1.txt'
+\copy (SELECT * FROM t3 ORDER BY i) to 'dump_t3_n1.txt'
+\copy (SELECT * FROM t4 ORDER BY i) to 'dump_t4_n1.txt'
+\copy (SELECT * FROM t5 ORDER BY i) to 'dump_t5_n1.txt'
 EOF
 
 $PSQL -p 11000 test <<EOF
@@ -67,8 +83,8 @@ $PSQL -p 11001 test <<EOF
 EOF
 
 # check if database contents are identical
-cat dump_t0.txt dump_s0.txt > dump0.txt 
-cat dump_t1.txt dump_s1.txt > dump1.txt 
+cat dump_t1_n0.txt dump_t2_n0.txt dump_t3_n0.txt dump_t4_n0.txt dump_t5_n0.txt dump_s0.txt > dump0.txt
+cat dump_t1_n1.txt dump_t2_n1.txt dump_t3_n1.txt dump_t4_n1.txt dump_t5_n1.txt dump_s1.txt > dump1.txt
 diff dump0.txt dump1.txt
 if [ $? != 0 ];then
 	# contents are not identical
