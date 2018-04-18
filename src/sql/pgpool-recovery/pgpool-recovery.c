7f47596405d326c4cd493e1b112aeaf993d5a8f9
Tatsuo Ishii <ishii@postgresql.org>
2018-04-18 10:03:37 +0900

    Prevent pcp_recovery_node from recovering "unused" status node.
    This allowed to try to recover a node without configuration data,
    which leads to variety of problems. See discussion:
    https://www.pgpool.net/pipermail/pgpool-general/2018-March/006021.html
    for more details.
    
    Also I fixed pgpool_recovery function so that it quotes an empty
    string argument with double quotes. Without this, the argument is
    treated as if it does not exist, which was the source of the complain
    from the user.

--- src/sql/pgpool-recovery/pgpool-recovery.c
+++ src/sql/pgpool-recovery/pgpool-recovery.c
@@ -4,7 +4,7 @@
  *
  * pgpool-recovery: exec online recovery script from SELECT statement.
  *
- * Copyright (c) 2003-2013	PgPool Global Development Group
+ * Copyright (c) 2003-2018	PgPool Global Development Group
  *
  * Permission to use, copy, modify, and distribute this software and
  * its documentation for any purpose and without fee is hereby
@@ -83,13 +83,13 @@ pgpool_recovery(PG_FUNCTION_ARGS)
 	{
 		char *remote_port = DatumGetCString(DirectFunctionCall1(textout,
 														  PointerGetDatum(PG_GETARG_TEXT_P(3))));
-		snprintf(recovery_script, sizeof(recovery_script), "%s/%s %s %s %s %s",
+		snprintf(recovery_script, sizeof(recovery_script), "\\"%s/%s\\" \\"%s\\" \\"%s\\" \\"%s\\" \\"%s\\"",
 				 DataDir, script, DataDir, remote_host,
 				 remote_data_directory, remote_port);
 	}
 	else
 	{
-		snprintf(recovery_script, sizeof(recovery_script), "%s/%s %s %s %s",
+		snprintf(recovery_script, sizeof(recovery_script), "\\"%s/%s\\" \\"%s\\" \\"%s\"" \\"%s\\"",
 				 DataDir, script, DataDir, remote_host,
 				 remote_data_directory);
 	}
