181b300060d03c3954db059c82a687a5e4148a31
Tatsuo Ishii <ishii@postgresql.org>
2018-06-12 15:21:52 +0900

    Fix 055.backend_all_down test failure.
    The test fails because pgpool zombie child process remains. Actually
    the failover process is properly performed but when the shutdown
    script is executed in background, output to stdout/stderr was blocked,
    and this could cause the zombie process syndrome. Soltution is,
    redirecting stdout/stderr to /dev/null when sponing the shutdown
    script in background.

--- src/test/regression/tests/055.backend_all_down/test.sh
+++ src/test/regression/tests/055.backend_all_down/test.sh
@@ -35,15 +35,23 @@ while true
 do
 	grep "failover done. shutdown host $PGSOCKET_DIR(11003)" log/pgpool.log > /dev/null 2>&1
 	if [ $? = 0 ];then
+	        echo "shutdown detected"
 		break;
 	fi
+
+	grep "failover: no backends are degenerated" log/pgpool.log > /dev/null 2>&1
+	if [ $? = 0 ];then
+	        echo "no backend to degenerate detected"
+		break;
+	fi
+
 	sleep 1
 done
 
 # if that's the case, shutdownall will hang.
-(./shutdownall)&
+(./shutdownall) >/dev/null 2>&1 &
 sleep 5
-kill $!
+kill $pid
 
 if [ $? = 0 ];then
 	./shutdownall
