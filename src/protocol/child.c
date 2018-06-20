a73a255edda9f051e8efc07fb1bab6defbbf433c
Tatsuo Ishii <ishii@postgresql.org>
2018-06-20 10:25:23 +0900

    Fix memory leaks related to pool_extract_error_message().
    After 3.4, the function starts to return palloc'ed memory, and the
    caller should had been modified so that they pfree the memory returned
    by it but actually they had not.

--- src/protocol/child.c
+++ src/protocol/child.c
@@ -282,7 +282,8 @@ void do_child(int *fds)
 		StartupPacket *sp;
 		int front_end_fd;
 		SockAddr saddr;
-		/* rest per iteration memory context */
+
+		/* reset per iteration memory context */
 		MemoryContextSwitchTo(ProcessLoopContext);
 		MemoryContextResetAndDeleteChildren(ProcessLoopContext);
 
