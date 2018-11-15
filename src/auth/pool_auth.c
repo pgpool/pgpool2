e00426d2d772a8c9db0cd8159e73ab402d20c2d1
Muhammad Usama <m.usama@gmail.com>
2018-11-16 01:48:31 +0500

    Fix broken authentication for Pgpool's internal connections
    The issue is caused by a  mistake in "SCRAM and Certificate authentication
    support" commit.
    The problem is while authenticating against backend in connection_do_auth(), it
    returns to caller as soon as backend returns auth ok response.  So
    authentication itself established fine. However connection_do_auth()
    does not proceed until it receives "Ready for query", it is required
    according to the frontend/backend protocol.
    The fix is to keep processing the data after receiving auth_ok response
    until we get ready for query.
    
    Patch provided by Tatsuo Ishii <ishii@sraoss.co.jp> and a tiny modification
    made by me

--- src/auth/pool_auth.c
+++ src/auth/pool_auth.c
@@ -153,7 +153,6 @@ connection_do_auth(POOL_CONNECTION_POOL_SLOT * cp, char *password)
 					 errdetail("backend replied with invalid kind")));
 
 		cp->con->auth_kind = AUTH_REQ_OK;
-		return;
 	}
 	else if (auth_kind == AUTH_REQ_CRYPT)	/* crypt password? */
 	{
@@ -177,7 +176,6 @@ connection_do_auth(POOL_CONNECTION_POOL_SLOT * cp, char *password)
 					 errdetail("backend replied with invalid kind")));
 
 		cp->con->auth_kind = AUTH_REQ_OK;
-		return;
 	}
 	else if (auth_kind == AUTH_REQ_MD5) /* md5 password? */
 	{
@@ -204,7 +202,6 @@ connection_do_auth(POOL_CONNECTION_POOL_SLOT * cp, char *password)
 					 errdetail("backend replied with invalid kind")));
 
 		cp->con->auth_kind = AUTH_REQ_OK;
-		return;
 	}
 	else if (auth_kind == AUTH_REQ_SASL)
 	{
@@ -216,6 +213,7 @@ connection_do_auth(POOL_CONNECTION_POOL_SLOT * cp, char *password)
 		}
 		ereport(DEBUG1,
 				(errmsg("SCRAM authentication successful for user:%s", cp->sp->user)));
+		cp->con->auth_kind = AUTH_REQ_OK;
 	}
 	else
 	{
