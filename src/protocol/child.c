e765df5a22cc32acd157105731cbcfcd714a8d76
Tatsuo Ishii <ishii@sraoss.co.jp>
2019-04-16 15:48:44 +0900

    Fix DROP DATABASE failure.
    When DROP DATABASE gets executed, SIGUSR1 is sent to the Pgpool-II
    child process being issuing the command. In its SIGUSR1 handler,
    MASTER macro is called while closing all idle connections. The MACRO
    checks whether we are in failover process surely we are. As a result,
    the process exits and DROP DATABASE command never been issued.
    
    Per bug 486. However the reason of segfault in the report is not
    clear.  After commit:
    https://git.postgresql.org/gitweb/?p=pgpool2.git;a=commit;h=66b5aacfcc045ec1485921a5884b637fcfb6fd73
    
    Things could be different. Let the user test the latest version in the
    git repo and see if the problem is solved...

--- src/protocol/child.c
+++ src/protocol/child.c
@@ -91,11 +91,15 @@ static void child_will_go_down(int code, Datum arg);
 static int opt_sort(const void *a, const void *b);
 
 /*
- * non 0 means SIGTERM(smart shutdown) or SIGINT(fast shutdown) has arrived
+ * Non 0 means SIGTERM (smart shutdown) or SIGINT (fast shutdown) has arrived
  */
 volatile sig_atomic_t exit_request = 0;
 static volatile sig_atomic_t alarm_enabled = false;
 
+/*
+ * Ignore SIGUSR1 if requested. Used when DROP DATABASE is requested.
+ */
+volatile sig_atomic_t ignore_sigusr1 = 0;
 
 static int	idle;				/* non 0 means this child is in idle state */
 static int	accepted = 0;
@@ -1075,6 +1079,12 @@ static RETSIGTYPE close_idle_connection(int sig)
 	POOL_CONNECTION_POOL *p = pool_connection_pool;
 	ConnectionInfo *info;
 	int			save_errno = errno;
+
+	/*
+	 * DROP DATABSE is ongoing.
+	 */
+	if (ignore_sigusr1)
+		return;
 
 #ifdef NOT_USED
 	ereport(DEBUG1,
