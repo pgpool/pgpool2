afbcff28985ef2965d8466c69da4a54b5381c17d
Tatsuo Ishii <ishii@sraoss.co.jp>
2019-05-03 09:02:29 +0900

    Speed up failover when all of backends are down.
    Pgpool-II tries to find primary node till search_primary_node_timeout
    expires even if all of the backend are in down status. This is not
    only a waste of time but makes Pgpool-II looked like hanged because
    while searching primary node failover process is suspended and all of
    the Pgpool-II child process are in defunct state, thus there's no
    process which accepts connection requests from clients. Since the
    default value of searching primary is 300 seconds, typically this
    keeps on for 300 seconds. This is not comfortable for users.
    
    So immediately give up finding primary node regardless
    search_primary_node_timeout and promptly finish the failover process
    if all of the backend are in down status.
    
    Discussion: https://www.pgpool.net/pipermail/pgpool-hackers/2019-May/003321.html

--- src/main/pgpool_main.c
+++ src/main/pgpool_main.c
@@ -2707,6 +2707,7 @@ static int find_primary_node_repeatedly(void)
 {
 	int sec;
 	int node_id = -1;
+	int			i;
 
 	/* Streaming replication mode? */
 	if (pool_config->master_slave_mode == 0 ||
@@ -2717,6 +2718,22 @@ static int find_primary_node_repeatedly(void)
 		 */
 		ereport(DEBUG1, 
 			(errmsg("find_primary_node: not in streaming replication mode")));
+		return -1;
+	}
+
+	/*
+	 * If all of the backends are down, there's no point to keep on searching
+	 * primary node.
+	 */
+	for (i = 0; i < NUM_BACKENDS; i++)
+	{
+		if (VALID_BACKEND(i))
+			break;
+	}
+	if (i == NUM_BACKENDS)
+	{
+		ereport(LOG,
+				(errmsg("find_primary_node_repeatedly: all of the backends are down. Giving up finding primary node")));
 		return -1;
 	}
 
