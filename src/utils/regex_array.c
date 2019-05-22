6fe1ebcb5ffcc6ed89b9862d31aebd823591edd1
Tatsuo Ishii <ishii@sraoss.co.jp>
2019-05-22 16:38:53 +0900

    Revert "Fix memory leak pointed out by coverity."
    This reverts commit 66bb6b7186edb56e8d567d0b9e7bf4fee8b6398c.

--- src/utils/regex_array.c
+++ src/utils/regex_array.c
@@ -209,7 +209,6 @@ void extract_string_tokens2(char *str, char *delimi, char delimi2, Left_right_to
 			pfree(left_token);
 			pfree(right_token);
 			pfree(mystr);
-			pfree(weight_token);
 			return;
 		}
 
@@ -230,7 +229,6 @@ void extract_string_tokens2(char *str, char *delimi, char delimi2, Left_right_to
 		lrtokens->token[lrtokens->pos].left_token = left_token;
 		lrtokens->token[lrtokens->pos].right_token = right_token;
 		lrtokens->pos++;
-		pfree(weight_token);
 	}
 	pfree(mystr);
 }
