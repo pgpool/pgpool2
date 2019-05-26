pgpool/pgpool2

commit 3dd1cd3f15287ee6bb8b09f0642f99db98e9776a

Fix for [pgpool-hackers: 3295] duplicate failover request ...
Pgpool should keep the backend health check running on quarantined nodes so
that when the connectivity resumes, they should automatically get removed
from the quarantine. Otherwise the temporary network glitch could send the node
into permanent quarantine state.

src/main/health_check.c
@@ -178,6 +178,7 @@ do_health_check_child(int *node_id)
 		else if (pool_config->health_check_params[*node_id].health_check_period > 0)
 		{
 			bool		result;
+			BackendInfo *bkinfo = pool_get_node_info(*node_id);
 
 			result = establish_persistent_connection(*node_id);
 
@@ -200,10 +201,26 @@ do_health_check_child(int *node_id)
 					ereport(LOG, (errmsg("health check failed on node %d (timeout:%d)",
 										 *node_id, health_check_timer_expired)));
 
-					/* trigger failover */
-					partial = health_check_timer_expired ? false : true;
-					degenerate_backend_set(node_id, 1, partial ? REQ_DETAIL_SWITCHOVER : 0);
+					if (bkinfo->backend_status == CON_DOWN && bkinfo->quarantine == true)
+					{
+						ereport(LOG, (errmsg("health check failed on quarantine node %d (timeout:%d)",
+											 *node_id, health_check_timer_expired),
+									  errdetail("ignoring..")));
+					}
+					else
+					{
+						/* trigger failover */
+						partial = health_check_timer_expired ? false : true;
+						degenerate_backend_set(node_id, 1, partial ? REQ_DETAIL_SWITCHOVER : 0);
+					}
 				}
+			}
+			else if (bkinfo->backend_status == CON_DOWN && bkinfo->quarantine == true)
+			{
+				/* The node has become reachable again. Reset
+				 * the quarantine state
+				 */
+				send_failback_request(*node_id, false, REQ_DETAIL_UPDATE | REQ_DETAIL_WATCHDOG);
 			}
 
 			/* Discard persistent connections */
@@ -228,9 +245,12 @@ establish_persistent_connection(int node)
 
 	/*
 	 * If the node is already in down status or unused, do nothing.
+	 * except when the node state is down because of quarantine operation
+	 * since we want to detect when the node cames back to life again to
+	 * remove it from the quarantine state
 	 */
 	if (bkinfo->backend_status == CON_UNUSED ||
-		bkinfo->backend_status == CON_DOWN)
+		(bkinfo->backend_status == CON_DOWN && bkinfo->quarantine == false))
 		return false;
 
 	/*

commit c7d359630428443775bffb57bfed25a6b6dc65e6

Enhancing the debugging/testing aid for health check.
To cater the continuous health check on quarantine nodes the commit extends
the check_backend_down_request() function to check for nodes that are marked
as "already_down" in the backend_down_request file. This should fix
013.watchdog_failover_require_consensus test case, that started failing
after 3dd1cd3f15287ee6bb8b09f0642f99db98e9776a commit.

src/main/health_check.c
@@ -80,7 +80,7 @@ static RETSIGTYPE health_check_timer_handler(int sig);
 #endif
 
 #ifdef HEALTHCHECK_DEBUG
-static bool check_backend_down_request(int node);
+static bool check_backend_down_request(int node, bool done_requests);
 #endif
 
 #undef CHECK_REQUEST
@@ -183,7 +183,7 @@ do_health_check_child(int *node_id)
 			result = establish_persistent_connection(*node_id);
 
 #ifdef HEALTHCHECK_DEBUG
-			if (check_backend_down_request(*node_id) || (result && slot == NULL))
+			if (check_backend_down_request(*node_id, false) || (result && slot == NULL))
 #else
 			if (result && slot == NULL)
 #endif
@@ -220,7 +220,10 @@ do_health_check_child(int *node_id)
 				/* The node has become reachable again. Reset
 				 * the quarantine state
 				 */
-				send_failback_request(*node_id, false, REQ_DETAIL_UPDATE | REQ_DETAIL_WATCHDOG);
+#ifdef HEALTHCHECK_DEBUG
+				if (check_backend_down_request(*node_id, true) == false)
+#endif
+					send_failback_request(*node_id, false, REQ_DETAIL_UPDATE | REQ_DETAIL_WATCHDOG);
 			}
 
 			/* Discard persistent connections */
@@ -420,10 +423,13 @@ static RETSIGTYPE health_check_timer_handler(int sig)
  * Check backend down request file with specified backend node id.  If it's
  * down ("down"), returns true and set the status to "already_down" to
  * prevent repeatable * failover. If it's other than "down", returns false.
-*/
+ *
+ * When done_requests is true (second arg to function) the function returns
+ * true if the node has already_done status in the file.
+ */
 
 static bool
-check_backend_down_request(int node)
+check_backend_down_request(int node, bool done_requests)
 {
 	static char backend_down_request_file[POOLMAXPATHLEN];
 	FILE	   *fd;
@@ -465,11 +471,20 @@ check_backend_down_request(int node)
 		strncpy(buf, readbuf, sizeof(buf));
 		if (strlen(readbuf) > 0 && readbuf[strlen(readbuf) - 1] == '\n')
 			buf[strlen(readbuf) - 1] = '\0';
+		sscanf(buf, "%d\t%s", &node_id, status);
 
+		if (done_requests)
+		{
+			if (node_id == node && !strcmp(status, "already_down"))
+			{
+				fclose(fd);
+				return true;
+			}
+			continue;
+		}
 		p = readbuf;
 		if (found == false)
 		{
-			sscanf(buf, "%d\t%s", &node_id, status);
 			if (node_id == node && !strcmp(status, "down"))
 			{
 				snprintf(linebuf, sizeof(linebuf), "%d\t%s\n", node_id, "already_down");