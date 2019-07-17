4600ed129e584f85832b4e5dcae7102fe15b97be
Tatsuo Ishii <ishii@sraoss.co.jp>
2019-07-17 16:51:31 +0900

    Fix the failover() so that it does not access out of array.
    Per Coverity.

--- src/main/pgpool_main.c
+++ src/main/pgpool_main.c
@@ -1783,6 +1783,7 @@ static void failover(void)
 			{
 				/* only if the failover is against the current primary */
 				if (((reqkind == NODE_DOWN_REQUEST) &&
+					 Req_info->primary_node_id >= 0 &&
 					 (nodes[Req_info->primary_node_id])) ||
 					(node_id >= 0 && (reqkind == PROMOTE_NODE_REQUEST) &&
 					 (VALID_BACKEND(node_id))))
