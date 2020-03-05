4803f6ef0a59209fbbc997b109125d194ab60c1f
Bo Peng <pengbo@sraoss.co.jp>
2020-03-06 08:00:18 +0900

    Update pool_config.c.

--- src/config/pool_config.c
+++ src/config/pool_config.c
@@ -1595,7 +1595,7 @@ YY_BUFFER_STATE yy_scan_bytes  (yyconst char * yybytes, yy_size_t  _yybytes_len 
 	YY_BUFFER_STATE b;
 	char *buf;
 	yy_size_t n;
-	int i;
+	yy_size_t i;
     
 	/* Get memory for full buffer, including space for trailing EOB's. */
 	n = _yybytes_len + 2;
