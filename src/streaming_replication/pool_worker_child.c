2585e6e32268230d7946a036527ebe6dd8b8773b
Tatsuo Ishii <ishii@sraoss.co.jp>
2020-05-16 09:05:56 +0900

    Remove more duplicate global data.
    This causes a link error on some system (Fedra 32).
    
    Discussion: https://www.pgpool.net/pipermail/pgpool-hackers/2020-April/003593.html

--- src/streaming_replication/pool_worker_child.c
+++ src/streaming_replication/pool_worker_child.c
@@ -3,7 +3,7 @@
  * pgpool: a language independent connection pool server for PostgreSQL
  * written by Tatsuo Ishii
  *
- * Copyright (c) 2003-2018	PgPool Global Development Group
+ * Copyright (c) 2003-2020	PgPool Global Development Group
  *
  * Permission to use, copy, modify, and distribute this software and
  * its documentation for any purpose and without fee is hereby
@@ -61,7 +61,6 @@
 #include "auth/pool_hba.h"
 #include "utils/pool_stream.h"
 
-char		remote_ps_data[NI_MAXHOST]; /* used for set_ps_display */
 static POOL_CONNECTION_POOL_SLOT * slots[MAX_NUM_BACKENDS];
 static volatile sig_atomic_t reload_config_request = 0;
 static volatile sig_atomic_t restart_request = 0;
