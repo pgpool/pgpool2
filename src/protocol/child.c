7587851563204a99154c98a69160dd137e977024
Takuma Hoshiai <hoshiai@sraoss.co.jp>
2020-06-22 11:40:24 +0900

    Fix miscount of connection_count
    There is a possibility that  pgpool duplicate the count down of conections
    when query cancel such as pg_terminate_backend() commmand.

--- src/protocol/child.c
+++ src/protocol/child.c
@@ -239,7 +239,10 @@ void do_child(int *fds)
 		MemoryContextSwitchTo(ProcessLoopContext);
 
 		if (accepted)
+		{
+			accepted = 0;
 			connection_count_down();
+		}
         
         backend_cleanup(&child_frontend, backend, frontend_invalid);
 
