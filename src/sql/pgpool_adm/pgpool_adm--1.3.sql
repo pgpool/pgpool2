cb34604d621f9890c15dae6ba81f6b94ed97e022
Tatsuo Ishii <ishii@sraoss.co.jp>
2020-07-07 08:32:22 +0900

    Allow to use argument names in pgpool_adm functions.
    Now pgpool_adm functions allow to use named parameters like:
    
    SELECT * FROM pcp_node_count(host => 'localhost', port => 11001, username => 't-ishii', password => 't-ishii');
    
    Patch provided by Hou, Zhijie, minor tweak by me.
    Discussion: https://www.pgpool.net/pipermail/pgpool-hackers/2020-July/003687.html

--- src/sql/pgpool_adm/pgpool_adm--1.3.sql
+++ src/sql/pgpool_adm/pgpool_adm--1.3.sql
@@ -7,15 +7,15 @@
 /**
  * input parameters: node_id, host, port, username, password
  */
-CREATE FUNCTION pcp_node_info(integer, text, integer, text, text, OUT host text, OUT port integer, OUT status text, OUT weight float4, OUT role text, OUT replication_delay bigint, OUT replication_state text, OUT replication_sync_state text, OUT last_status_change timestamp)
+CREATE FUNCTION pcp_node_info(IN node_id integer, IN host text, IN port integer, IN username text, IN password text, OUT host text, OUT port integer, OUT status text, OUT weight float4, OUT role text, OUT replication_delay bigint, OUT replication_state text, OUT replication_sync_state text, OUT last_status_change timestamp)
 RETURNS record
 AS 'MODULE_PATHNAME', '_pcp_node_info'
 LANGUAGE C VOLATILE STRICT;
 
 /**
- * input parameters: node_id, server_name
+ * input parameters: node_id, pcp_server
  */
-CREATE FUNCTION pcp_node_info(integer, text, OUT host text, OUT port integer, OUT status text, OUT weight float4)
+CREATE FUNCTION pcp_node_info(IN node_id integer, IN pcp_server text, OUT host text, OUT port integer, OUT status text, OUT weight float4)
 RETURNS record
 AS 'MODULE_PATHNAME', '_pcp_node_info'
 LANGUAGE C VOLATILE STRICT;
@@ -23,15 +23,15 @@ LANGUAGE C VOLATILE STRICT;
 /**
  * input parameters: node_id, host, port, username, password
  */
-CREATE FUNCTION pcp_health_check_stats(integer, text, integer, text, text, OUT node_id integer, OUT host text, OUT port integer, OUT status text, OUT role text, OUT last_status_change timestamp, OUT total_count bigint, OUT success_count bigint, OUT fail_count bigint, OUT skip_count bigint, OUT retry_count bigint, OUT average_retry_count float4, OUT max_retry_count bigint, OUT max_health_check_duration bigint, OUT min_health_check_duration bigint, OUT average_health_check_duration float4, OUT last_health_check timestamp, OUT last_successful_health_check timestamp, OUT last_skip_health_check timestamp, OUT last_failed_health_check timestamp)
+CREATE FUNCTION pcp_health_check_stats(IN node_id integer, IN host text, IN port integer, IN username text, IN password text, OUT node_id integer, OUT host text, OUT port integer, OUT status text, OUT role text, OUT last_status_change timestamp, OUT total_count bigint, OUT success_count bigint, OUT fail_count bigint, OUT skip_count bigint, OUT retry_count bigint, OUT average_retry_count float4, OUT max_retry_count bigint, OUT max_health_check_duration bigint, OUT min_health_check_duration bigint, OUT average_health_check_duration float4, OUT last_health_check timestamp, OUT last_successful_health_check timestamp, OUT last_skip_health_check timestamp, OUT last_failed_health_check timestamp)
 RETURNS record
 AS 'MODULE_PATHNAME', '_pcp_health_check_stats'
 LANGUAGE C VOLATILE STRICT;
 
 /**
- * input parameters: node_id, server_name
+ * input parameters: node_id, pcp_server
  */
-CREATE FUNCTION pcp_health_check_stats(integer, text, OUT host text, OUT port integer, OUT status text, OUT weight float4)
+CREATE FUNCTION pcp_health_check_stats(IN node_id integer, IN pcp_server text, OUT host text, OUT port integer, OUT status text, OUT weight float4)
 RETURNS record
 AS 'MODULE_PATHNAME', '_pcp_health_check_stats'
 LANGUAGE C VOLATILE STRICT;
@@ -39,15 +39,15 @@ LANGUAGE C VOLATILE STRICT;
 /**
  * input parameters: host, port, username, password
  */
-CREATE FUNCTION pcp_pool_status(text, integer, text, text, OUT item text, OUT value text, OUT description text)
+CREATE FUNCTION pcp_pool_status(IN host text, IN port integer, IN username text, IN password text, OUT item text, OUT value text, OUT description text)
 RETURNS record
 AS 'MODULE_PATHNAME', '_pcp_pool_status'
 LANGUAGE C VOLATILE STRICT;
 
 /**
- * input parameters: server_name
+ * input parameters: pcp_server
  */
-CREATE FUNCTION pcp_pool_status(text, OUT item text, OUT value text, OUT description text)
+CREATE FUNCTION pcp_pool_status(IN pcp_server text, OUT item text, OUT value text, OUT description text)
 RETURNS record
 AS 'MODULE_PATHNAME', '_pcp_pool_status'
 LANGUAGE C VOLATILE STRICT;
@@ -55,15 +55,15 @@ LANGUAGE C VOLATILE STRICT;
 /**
  * input parameters: host, port, username, password
  */
-CREATE FUNCTION pcp_node_count(text, integer, text, text, OUT node_count integer)
+CREATE FUNCTION pcp_node_count(IN host text, IN port integer, IN username text, IN password text, OUT node_count integer)
 RETURNS integer
 AS 'MODULE_PATHNAME', '_pcp_node_count'
 LANGUAGE C VOLATILE STRICT;
 
 /**
- * input parameters: server_name
+ * input parameters: pcp_server
  */
-CREATE FUNCTION pcp_node_count(text, OUT node_count integer)
+CREATE FUNCTION pcp_node_count(IN pcp_server text, OUT node_count integer)
 RETURNS integer
 AS 'MODULE_PATHNAME', '_pcp_node_count'
 LANGUAGE C VOLATILE STRICT;
@@ -71,15 +71,15 @@ LANGUAGE C VOLATILE STRICT;
 /**
  * input parameters: node_id, host, port, username, password
  */
-CREATE FUNCTION pcp_attach_node(integer, text, integer, text, text, OUT node_attached boolean)
+CREATE FUNCTION pcp_attach_node(IN node_id integer, IN host text, IN port integer, IN username text, IN password text, OUT node_attached boolean)
 RETURNS boolean
 AS 'MODULE_PATHNAME', '_pcp_attach_node'
 LANGUAGE C VOLATILE STRICT;
 
 /**
- * input parameters: node_id, server_name
+ * input parameters: node_id, pcp_server
  */
-CREATE FUNCTION pcp_attach_node(integer, text, OUT node_attached boolean)
+CREATE FUNCTION pcp_attach_node(IN node_id integer, IN pcp_server text, OUT node_attached boolean)
 RETURNS boolean
 AS 'MODULE_PATHNAME', '_pcp_attach_node'
 LANGUAGE C VOLATILE STRICT;
@@ -87,15 +87,15 @@ LANGUAGE C VOLATILE STRICT;
 /**
  * input parameters: node_id, gracefully, host, port, username, password
  */
-CREATE FUNCTION pcp_detach_node(integer, boolean, text, integer, text, text, OUT node_detached boolean)
+CREATE FUNCTION pcp_detach_node(IN node_id integer, IN gracefully boolean, IN host text, IN port integer, IN username text, IN password text, OUT node_detached boolean)
 RETURNS boolean
 AS 'MODULE_PATHNAME', '_pcp_detach_node'
 LANGUAGE C VOLATILE STRICT;
 
 /**
- * input parameters: node_id, gracefully, server_name
+ * input parameters: node_id, gracefully, pcp_server
  */
-CREATE FUNCTION pcp_detach_node(integer, boolean, text, OUT node_detached boolean)
+CREATE FUNCTION pcp_detach_node(IN node_id integer, IN gracefully boolean, IN pcp_server text, OUT node_detached boolean)
 RETURNS boolean
 AS 'MODULE_PATHNAME', '_pcp_detach_node'
 LANGUAGE C VOLATILE STRICT;
