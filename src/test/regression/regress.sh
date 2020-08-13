5ed981877ca38a19101b9afdf30ac794f126fdc6
Bo Peng <pengbo@sraoss.co.jp>
2020-08-13 15:35:34 +0900

    Before running regression test clean the remaining processes and sockets.

--- src/test/regression/regress.sh
+++ src/test/regression/regress.sh
@@ -168,6 +168,7 @@ fi
 verify_pginstallation
 export_env_vars
 print_info
+source $TESTLIBS
 
 #Start executing tests
 rm -fr $log
@@ -185,6 +186,7 @@ for i in $dirs
 do
 	cd $i
 	echo -n "testing $i..."
+	clean_all
 	timeout $TIMEOUT ./test.sh > $log/$i 2>&1
 	rtn=$?
 	if [ $rtn = 0 ];then
