4b35c0cdc23e4b22700a850d536f9b16f34735f2
Tatsuo Ishii <ishii@sraoss.co.jp>
2020-08-14 14:15:22 +0900

    Check panic or fatal error by using 'V' token if available.
    To identify panic or fatal message in is_panic_or_fatal_error(), 'S'
    token was used.  However 'S' could be localized and simply comparing
    "PANIC" or "FATAL" with the message may not work. Instead 'V' should be
    used if possible because it's not localized.

--- src/protocol/pool_process_query.c
+++ src/protocol/pool_process_query.c
@@ -4050,6 +4050,10 @@ static bool is_panic_or_fatal_error(const char *message, int major)
 			if (id == '\0')
 				break;
 
+			/* V is never localized. Only available 9.6 or later. */
+			if (id == 'V' && (strcasecmp("PANIC", message) == 0 || strcasecmp("FATAL", message) == 0))
+				return true;
+
 			if (id == 'S' && (strcasecmp("PANIC", message) == 0 || strcasecmp("FATAL", message) == 0))
 				return true;
 			else
