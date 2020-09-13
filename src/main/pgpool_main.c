7bf7f0925a02f59c0805c39e85e95908fa92999f
Tatsuo Ishii <ishii@sraoss.co.jp>
2020-09-14 08:09:18 +0900

    Remove unnecessary checks in some code path.
    Patch contributed by Hou, Zhijie.
    Discussion: https://www.pgpool.net/pipermail/pgpool-hackers/2020-September/003805.html

--- src/main/pgpool_main.c
+++ src/main/pgpool_main.c
@@ -2115,16 +2115,14 @@ static void failover(void)
 			sts = waitpid(pcp_pid, &status, 0);
 			if (sts != -1)
 				break;
-			if (sts == -1)
+
+			if (errno == EINTR)
+				continue;
+			else
 			{
-				if (errno == EINTR)
-					continue;
-				else
-				{
-					ereport(WARNING,
-							(errmsg("failover: waitpid failed. reason: %s", strerror(errno))));
-					continue;
-				}
+				ereport(WARNING,
+						(errmsg("failover: waitpid failed. reason: %s", strerror(errno))));
+				continue;
 			}
 		}
 		if (WIFSIGNALED(status))
