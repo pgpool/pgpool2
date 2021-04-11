8c51c3a78c209705dc7898c61c949365fb3d2a53
Tatsuo Ishii <ishii@sraoss.co.jp>
2021-04-11 14:25:31 +0900

    Add new regression test 074.bug700_memqcache_bug_segfault_at_close_complete which was missed in the previous commit.
    The test was missed in commit: a531e783c90a88ab429d0de83fadb7e41c787a92

---
+++ src/test/regression/tests/074.bug700_memqcache_bug_segfault_at_close_complete/test.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------
+# test script for a memqcache bug 700.
+#
+# Pgpool-II crashes upon receiving CloseComplete.
+# This only happens in other than streaming and logical replication mode.
+
+source $TESTLIBS
+WHOAMI=`whoami`
+TESTDIR=testdir
+PGPROTO=pgproto
+
+for mode in s i r n
+do
+    rm -fr $TESTDIR
+    mkdir $TESTDIR
+    cd $TESTDIR
+
+    # create test environment
+    echo -n "creating test environment in mode = $mode..."
+    $PGPOOL_SETUP -m $mode -n 1 || exit 1
+    echo "done."
+
+    source ./bashrc.ports
+
+    export PGPORT=$PGPOOL_PORT
+
+    echo "memory_cache_enabled = on" >> etc/pgpool.conf
+    ./startall
+    wait_for_pgpool_startup
+
+    $PGPROTO -d test -f ../pgproto.data > result.txt 2>&1
+    cmp ../expected.txt result.txt
+    if [ $? != 0 ];then
+	./shutdownall
+	exit 1
+    fi
+    ./shutdownall
+    cd ..
+done
+
+exit 0
