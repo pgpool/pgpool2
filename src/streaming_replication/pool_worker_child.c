cf232bf73afeb78cadd989b5503ed7ec311db00f
Tatsuo Ishii <ishii@sraoss.co.jp>
2021-05-14 11:44:47 +0900

    Update copyright year.

--- src/streaming_replication/pool_worker_child.c
+++ src/streaming_replication/pool_worker_child.c
@@ -3,7 +3,7 @@
  * pgpool: a language independent connection pool server for PostgreSQL
  * written by Tatsuo Ishii
  *
- * Copyright (c) 2003-2020	PgPool Global Development Group
+ * Copyright (c) 2003-2021	PgPool Global Development Group
  *
  * Permission to use, copy, modify, and distribute this software and
  * its documentation for any purpose and without fee is hereby
