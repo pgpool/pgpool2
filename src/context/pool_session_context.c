3a32b3b678a13cc63ecc7e82593c4bc0b9b863b4
Bo Peng <pengbo@sraoss.co.jp>
2021-08-18 17:00:28 +0900

    Fix the incorrect display of load balancing node in raw mode.
    In raw mode, Pgpool-II sends all queies to main node.
    This is harmless, but it may confuse users.

--- src/context/pool_session_context.c
+++ src/context/pool_session_context.c
@@ -96,7 +96,7 @@ pool_init_session_context(POOL_CONNECTION * frontend, POOL_CONNECTION_POOL * bac
 				(errmsg("failed to get process info for current process")));
 
 	/* Choose load balancing node if necessary */
-	if (pool_config->load_balance_mode)
+	if (!RAW_MODE && pool_config->load_balance_mode)
 	{
 		node_id = select_load_balancing_node();
 	}
