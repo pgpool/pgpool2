a086144ae2e7f6812bd3c0b66b8f34e1677ec974
Tatsuo Ishii <ishii@sraoss.co.jp>
2021-10-12 10:40:11 +0900

    Fix signal handler for SIGTERM, SIGINT and SIGQUIT.
    It did not properly save errno and it was possible to overwrite errno.

--- src/protocol/child.c
+++ src/protocol/child.c
@@ -1077,9 +1077,9 @@ static RETSIGTYPE die(int sig)
 {
 	int			save_errno;
 
-	POOL_SETMASK(&BlockSig);
-
 	save_errno = errno;
+
+	POOL_SETMASK(&BlockSig);
 
 #ifdef NOT_USED
 	ereport(LOG,
@@ -1125,8 +1125,9 @@ static RETSIGTYPE die(int sig)
 			break;
 	}
 
-	errno = save_errno;
 	POOL_SETMASK(&UnBlockSig);
+
+	errno = save_errno;
 }
 
 /*
