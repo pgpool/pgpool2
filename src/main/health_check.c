e3bbca3441c8d43cad8387b8daae91cf7f166a73
Tatsuo Ishii <ishii@sraoss.co.jp>
2021-12-22 10:13:20 +0900

    Allow to run regression test against existing installation without recompiling.
    It is possible to run regression test using existing installation.
    
    regression.sh -m noinstall
    
    However some of tests fail in this case because those tests require
    pgpool to be compiled with variable HEALTHCHECK_DEBUG is set. This is
    only possible in following procedure.
    
    make clean
    cd src/test/regression
    ./regress.sh
    
    To run the regression test against existing installation new config
    variable "health_check_test" is added. The source code is always
    compiled as if HEALTHCHECK_DEBUG is set. The test facility is not
    activated unless health_check_test is set to on.
    
    For now I push to only master branch. After some tests, I am going to
    push to all supported branches. I know adding new parameter to stable
    branches is unusual but the feature is for enhancing test and it is
    not visible to ordinal users. So I think my plan is justified by them.
    
    Discussion: https://www.pgpool.net/pipermail/pgpool-hackers/2021-December/004078.html

--- src/main/health_check.c
+++ src/main/health_check.c
@@ -5,7 +5,7 @@
  * pgpool: a language independent connection pool server for PostgreSQL
  * written by Tatsuo Ishii
  *
- * Copyright (c) 2003-2020	PgPool Global Development Group
+ * Copyright (c) 2003-2021	PgPool Global Development Group
  *
  * Permission to use, copy, modify, and distribute this software and
  * its documentation for any purpose and without fee is hereby
@@ -82,15 +82,7 @@ static RETSIGTYPE reload_config_handler(int sig);
 static void reload_config(void);
 static RETSIGTYPE health_check_timer_handler(int sig);
 
-#ifdef HEALTHCHECK_OPTS
-#if HEALTHCHECK_OPTS > 0
-#define HEALTHCHECK_DEBUG
-#endif
-#endif
-
-#ifdef HEALTHCHECK_DEBUG
 static bool check_backend_down_request(int node, bool done_requests);
-#endif
 
 #undef CHECK_REQUEST
 #define CHECK_REQUEST \
@@ -386,12 +378,18 @@ establish_persistent_connection(int node)
 				CLEAR_ALARM;
 			}
 
-#ifdef HEALTHCHECK_DEBUG
-			if (slot && check_backend_down_request(node, false) == true)
+			/*
+			 * If health check test is enabled, check if fake down request is
+			 * set.  If set, discard_persistent_connection will set slot to
+			 * NULL.  This simulates a connection failure.
+			 */
+			if (pool_config->health_check_test)
 			{
-				discard_persistent_connection(node);
+				if (slot && check_backend_down_request(node, false) == true)
+				{
+					discard_persistent_connection(node);
+				}
 			}
-#endif
 
 			if (slot)
 			{
@@ -564,8 +562,6 @@ health_check_stats_init(POOL_HEALTH_CHECK_STATISTICS *addr)
 	}
 }
 
-#ifdef HEALTHCHECK_DEBUG
-
 /*
  * Node down request file. In the file, each line consists of "backend node
  * id", tab and "down".  If such a line found, check_backend_down_request()
@@ -577,7 +573,7 @@ health_check_stats_init(POOL_HEALTH_CHECK_STATISTICS *addr)
 /*
  * Check backend down request file with specified backend node id.  If it's
  * down ("down"), returns true and set the status to "already_down" to
- * prevent repeatable * failover. If it's other than "down", returns false.
+ * prevent repeating failover. If it's other than "down", returns false.
  *
  * When done_requests is true (second arg to function) the function returns
  * true if the node has already_done status in the file.
@@ -668,29 +664,5 @@ check_backend_down_request(int node, bool done_requests)
 	if (!found)
 		return false;
 
-#ifdef NOT_USED
-	fd = fopen(backend_down_request_file, "w");
-	if (!fd)
-	{
-		ereport(WARNING,
-				(errmsg("check_backend_down_request: failed to open file for writing %s",
-						backend_down_request_file),
-				 errdetail("%m")));
-		return false;
-	}
-
-	if (fwrite(writebuf, 1, strlen(writebuf), fd) != strlen(writebuf))
-	{
-		ereport(WARNING,
-				(errmsg("check_backend_down_request: failed to write %s",
-						backend_down_request_file),
-				 errdetail("%m")));
-		fclose(fd);
-		return false;
-	}
-	fclose(fd);
-#endif
-
 	return true;
 }
-#endif
