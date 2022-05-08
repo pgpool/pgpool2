eea522ebfcf791a623e865deaa1aa6fb59e3c50b
Tatsuo Ishii <ishii@sraoss.co.jp>
2022-05-08 18:00:20 +0900

    Fix not to abort session while in failed transaction.
    When an explicit transaction fails, subsequent commands should be
    ignored until commit or rollback command comes in. However pgpool
    sometimes aborted the session if relcache lookup happened. example:
    
    test=# begin;
    BEGIN
    test=*# insert into ttt values(1);
    ERROR:  relation "ttt" does not exist
    LINE 1: insert into ttt values(1);
                        ^
    test=!# select * from t1;
    FATAL:  Backend throw an error message
    DETAIL:  Exiting current session because of an error from backend
    HINT:  BACKEND Error: "current transaction is aborted, commands ignored until end of transaction block"
    server closed the connection unexpectedly
    	This probably means the server terminated abnormally
    	before or while processing the request.
    The connection to the server was lost. Attempting reset: Succeeded.
    
    To prevent  this, check the transaction  state and if it's  already in
    aborted state, do not try to  process the command, but just reply back
    to frontend with an error message:
    
    ERROR:  current transaction is aborted, commands ignored until end of transaction block
    
    By the way, while implementing the fix, I found old bugs with replication mode.
    
    - ReadyForQuery() mistakenly called end_internal_transaction() even if
      it's not in replication/si mode. Moreover,
      end_internal_transaction() is called even if the internal
      transaction has not started.
    
    - end_internal_transaction() did not call pool_unset_failed_transaction();
    
    These bugs were harmless until I tried to fix the issue (session
    aborting).
    
    New regression test 078 for this added.
    
    Discussion: https://www.pgpool.net/pipermail/pgpool-general/2022-April/008155.html

--- src/protocol/pool_process_query.c
+++ src/protocol/pool_process_query.c
@@ -4203,6 +4203,7 @@ end_internal_transaction(POOL_CONNECTION * frontend, POOL_CONNECTION_POOL * back
 	PG_END_TRY();
 
 	POOL_SETMASK(&oldmask);
+	pool_unset_failed_transaction();
 	return POOL_CONTINUE;
 }
 
