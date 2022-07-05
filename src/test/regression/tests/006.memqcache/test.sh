2a6a721888454550ad9071b501cc77d4df3474cf
Tatsuo Ishii <ishii@sraoss.co.jp>
2022-07-05 14:06:10 +0900

    Fix query cache to not cache timestamptz and timetz cast.
    Even if query is constant + cast (like '2022-07-05
    14:07:00'::timestamptz), the result can be changed by SET TIME ZONE
    command etc. and the result should not be cached.
    Also regression test data added.
    
    Discussion: https://www.pgpool.net/pipermail/pgpool-general/2022-July/008353.html

--- src/test/regression/tests/006.memqcache/test.sh
+++ src/test/regression/tests/006.memqcache/test.sh
@@ -63,6 +63,16 @@ EXPLAIN ANALYZE INSERT INTO explain_analyze VALUES(1);
 SELECT * FROM explain_analyze;
 SELECT CURRENT_TIMESTAMP;
 SELECT CURRENT_USER;
+SELECT '2022-07-05 10:00:00'::TIMESTAMP;
+SELECT '2022-07-05 10:00:00'::TIMESTAMP;
+SELECT '2022-07-05 10:00:00'::TIME;
+SELECT '2022-07-05 10:00:00'::TIME;
+SELECT '2022-07-05 10:00:00'::DATE;
+SELECT '2022-07-05 10:00:00'::DATE;
+SELECT '2022-07-05 10:00:00'::TIMESTAMPTZ;
+SELECT '2022-07-05 10:00:00'::TIMESTAMPTZ;
+SELECT '2022-07-05 10:00:00'::TIMETZ;
+SELECT '2022-07-05 10:00:00'::TIMETZ;
 EOF
 
 	success=true
@@ -74,6 +84,12 @@ EOF
 	grep "fetched from cache" log/pgpool.log | grep explain_analyze > /dev/null && success=false
 	grep "fetched from cache" log/pgpool.log | grep CURRENT_TIMESTAMP > /dev/null && success=false
 	grep "fetched from cache" log/pgpool.log | grep CURRENT_USER > /dev/null && success=false
+	grep "fetched from cache" log/pgpool.log | grep 'TIMESTAMP;' > /dev/null || success=false
+	grep "fetched from cache" log/pgpool.log | grep 'TIME;' > /dev/null || success=false
+	grep "fetched from cache" log/pgpool.log | grep 'DATE;' > /dev/null || success=false
+	grep "fetched from cache" log/pgpool.log | grep 'TIMESTAMPTZ;' > /dev/null && success=false
+	grep "fetched from cache" log/pgpool.log | grep 'TIMETZ;' > /dev/null && success=false
+
 	if [ $success = false ];then
 		./shutdownall
 		exit 1
