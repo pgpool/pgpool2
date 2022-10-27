722ee1800029e3cb9cffbe5d70dcc8db7d50de71
Tatsuo Ishii <ishii@sraoss.co.jp>
2022-10-27 09:15:16 +0900

    Deal with idle_session_timeout.
    If idle_session_timeout (added in PostgreSQL 14) is enabled and the
    timeout fires, followings happen:
    
    - If failover_on_backend_error is on (the default), Pgpool-II will
      trigger failover.
    
    - If only one of PostgreSQL servers enables idle_session_timeout,
      Pgpool-II could hang.
    
    To deal with idle_session_timeout detect_idle_session_timeout_error()
    is added to detect the error code for idle_session_timeout. If the
    error is detected, Pgpool-II returns the error code to frondend as a
    fatal error and disconnects the session.  This is a similar fix
    implemented for idle_in_transaction_session_timeout.
    
    https://git.postgresql.org/gitweb/?p=pgpool2.git;a=commit;h=3f5986eee360f12e6a0bb77aa46f95abf5f6bc10
    
    Discussion: https://www.pgpool.net/pipermail/pgpool-hackers/2022-October/004209.html
    Back-path-through: 4.0

--- src/protocol/pool_process_query.c
+++ src/protocol/pool_process_query.c
@@ -71,6 +71,7 @@
 #define ADMIN_SHUTDOWN_ERROR_CODE "57P01"
 #define CRASH_SHUTDOWN_ERROR_CODE "57P02"
 #define IDLE_IN_TRANSACTION_SESSION_TIMEOUT_ERROR_CODE "25P03"
+#define IDLE_SESSION_TIMEOUT_ERROR_CODE "57P05"
 
 static int	reset_backend(POOL_CONNECTION_POOL * backend, int qcnt);
 static char *get_insert_command_table_name(InsertStmt *node);
@@ -4370,6 +4371,18 @@ detect_idle_in_transaction_sesion_timeout_error(POOL_CONNECTION * backend, int m
 	return r;
 }
 
+int
+detect_idle_session_timeout_error(POOL_CONNECTION * backend, int major)
+{
+	int			r = detect_error(backend, IDLE_SESSION_TIMEOUT_ERROR_CODE, major, 'E', true);
+
+	if (r == SPECIFIED_ERROR)
+		ereport(DEBUG1,
+				(errmsg("detecting idle session timeout error"),
+				 errdetail("idle session timeout error message from backend")));
+	return r;
+}
+
 /*
  * detect_error: Detect specified error from error code.
  * returns 0 in case of sucess or 1 in cause of specified error.
@@ -4898,7 +4911,18 @@ SELECT_RETRY:
 				{
 					ereport(FATAL,
 							(pool_error_code(IDLE_IN_TRANSACTION_SESSION_TIMEOUT_ERROR_CODE),
-							 errmsg("terminating connection due to idle-in-transaction timeout")));
+							 errmsg("terminating connection due to idle-in-transaction session timeout")));
+				}
+
+				/*
+				 * connection was terminated due to idle_session_timeout expired
+				 */
+				r = detect_idle_session_timeout_error(CONNECTION(backend, i), MAJOR(backend));
+				if (r == SPECIFIED_ERROR)
+				{
+					ereport(FATAL,
+							(pool_error_code(IDLE_SESSION_TIMEOUT_ERROR_CODE),
+							 errmsg("terminating connection due to idle session timeout")));
 				}
 
 				/*
