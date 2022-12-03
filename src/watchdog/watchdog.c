91d86cbffeb2fc3b65fd4407d868d9bb96240232
Bo Peng <pengbo@sraoss.co.jp>
2022-12-03 22:31:15 +0900

    Downgrade LOG messages "new IPC connection received" to DEBUG1.
    It is a normal messages and should not be logged as LOG.
    Patch is created by pstef and reviewed by Bo Peng.

--- src/watchdog/watchdog.c
+++ src/watchdog/watchdog.c
@@ -3243,7 +3243,7 @@ accept_incomming_connections(fd_set *rmask, int pending_fds_count)
 		{
 			MemoryContext oldCxt = MemoryContextSwitchTo(TopMemoryContext);
 
-			ereport(LOG,
+			ereport(DEBUG1,
 					(errmsg("new IPC connection received")));
 			g_cluster.ipc_command_socks = lappend_int(g_cluster.ipc_command_socks, fd);
 			MemoryContextSwitchTo(oldCxt);
