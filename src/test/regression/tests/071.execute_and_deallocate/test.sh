dbdf058dabc8ddb5117d3e51459133cc51f67269
Tatsuo Ishii <ishii@sraoss.co.jp>
2023-01-28 11:37:18 +0900

    Test: fix indentation for further refactoring.

--- src/test/regression/tests/071.execute_and_deallocate/test.sh
+++ src/test/regression/tests/071.execute_and_deallocate/test.sh
@@ -14,37 +14,37 @@ success_count=0
 #for mode in s r n
 for mode in s
 do
-	rm -fr $TESTDIR
-	mkdir $TESTDIR
-	cd $TESTDIR
+    rm -fr $TESTDIR
+    mkdir $TESTDIR
+    cd $TESTDIR
 
-	# create test environment
-	echo -n "creating test environment..."
-	$PGPOOL_SETUP -m $mode -n 2 || exit 1
-	echo "done."
+    # create test environment
+    echo -n "creating test environment..."
+    $PGPOOL_SETUP -m $mode -n 2 || exit 1
+    echo "done."
 
-	source ./bashrc.ports
+    source ./bashrc.ports
 
-	export PGPORT=$PGPOOL_PORT
+    export PGPORT=$PGPOOL_PORT
 
-	# set backend_weight , loadbalance to slave only
-	echo "backend_weight0 = 0" >> etc/pgpool.conf
-	echo "backend_weight1 = 1" >> etc/pgpool.conf
+    # set backend_weight , loadbalance to standby only
+    echo "backend_weight0 = 0" >> etc/pgpool.conf
+    echo "backend_weight1 = 1" >> etc/pgpool.conf
 
-	# start pgpool-II
-	./startall
+    # start pgpool-II
+    ./startall
 
-	sleep 1
+    sleep 1
 
-	# run test1 select query
-	$PSQL -p 11000 test <<EOF
+    # run test1 select query
+    $PSQL -p 11000 test <<EOF
 PREPARE test1 AS SELECT 1;
 EXECUTE test1;
 DEALLOCATE test1;
 EOF
 
-	# run test2 update query
-	$PSQL -p 11000 test <<EOF
+    # run test2 update query
+    $PSQL -p 11000 test <<EOF
 CREATE TABLE test_tbl(id int, name text);
 PREPARE test2 AS UPDATE test_tbl SET id =2;
 EXECUTE test2;
@@ -52,48 +52,48 @@ DEALLOCATE test2;
 DEALLOCATE all;
 EOF
 
-	expect1=`fgrep "PREPARE test1" log/pgpool.log  | awk '{print substr($0, index($0, "DB node id:"),13)}'`
-	expect2=`fgrep "PREPARE test2" log/pgpool.log  | awk '{print substr($0, index($0, "DB node id:"),13)}'`
+    expect1=`fgrep "PREPARE test1" log/pgpool.log  | awk '{print substr($0, index($0, "DB node id:"),13)}'`
+    expect2=`fgrep "PREPARE test2" log/pgpool.log  | awk '{print substr($0, index($0, "DB node id:"),13)}'`
 
-	#test1 result
-	result=`fgrep "EXECUTE test1" log/pgpool.log  | awk '{print substr($0, index($0, "DB node id:"),13)}' `
-	if [  "$expect1" = "$result" ]; then
+    #test1 result
+    result=`fgrep "EXECUTE test1" log/pgpool.log  | awk '{print substr($0, index($0, "DB node id:"),13)}' `
+    if [  "$expect1" = "$result" ]; then
         success_count=$(( success_count + 1 ))
         echo "PREPARE and EXECUTE with SELECT query successfully."
-	fi
-	result=`fgrep "DEALLOCATE test1" log/pgpool.log  | awk '{print substr($0, index($0, "DB node id:"),13)}' `
-	if [  "$expect1" = "$result" ]; then
+    fi
+    result=`fgrep "DEALLOCATE test1" log/pgpool.log  | awk '{print substr($0, index($0, "DB node id:"),13)}' `
+    if [  "$expect1" = "$result" ]; then
         success_count=$(( success_count + 1 ))
         echo "PREPARE and DEALLOCATE with SELECT query successfully."
-	fi
+    fi
 
-	#test2 result
-	result=`fgrep "EXECUTE test2" log/pgpool.log  | awk '{print substr($0, index($0, "DB node id:"),13)}' `
-	if [  "$expect2" = "$result" ]; then
+    #test2 result
+    result=`fgrep "EXECUTE test2" log/pgpool.log  | awk '{print substr($0, index($0, "DB node id:"),13)}' `
+    if [  "$expect2" = "$result" ]; then
         success_count=$(( success_count + 1 ))
         echo "PREPARE and EXECUTE with UPDATE query successfully."
-	fi
-	result=`fgrep "DEALLOCATE test2" log/pgpool.log  | awk '{print substr($0, index($0, "DB node id:"),13)}' `
-	if [  "$expect2" = "$result" ]; then
+    fi
+    result=`fgrep "DEALLOCATE test2" log/pgpool.log  | awk '{print substr($0, index($0, "DB node id:"),13)}' `
+    if [  "$expect2" = "$result" ]; then
         success_count=$(( success_count + 1 ))
         echo "PREPARE and DEALLOCATE with UPDATE query successfully."
-	fi
+    fi
 
-	# DEALLOCATE all;
-	grep -E "DB node id: 0 .*DEALLOCATE all" log/pgpool.log
-	if [  $? -eq 0 ]; then
+    # DEALLOCATE all;
+    grep -E "DB node id: 0 .*DEALLOCATE all" log/pgpool.log
+    if [  $? -eq 0 ]; then
         success_count=$(( success_count + 1 ))
         echo "node0 DEALLOCATE all query successfully."
-	fi
-	grep -E "DB node id: 1 .*DEALLOCATE all" log/pgpool.log
-	if [  $? -eq 0 ]; then
+    fi
+    grep -E "DB node id: 1 .*DEALLOCATE all" log/pgpool.log
+    if [  $? -eq 0 ]; then
         success_count=$(( success_count + 1 ))
         echo "node1 DEALLOCATE all query successfully."
-	fi
+    fi
 
-	./shutdownall
+    ./shutdownall
 
-	cd ..
+    cd ..
 done
 
 echo "$success_count out of $num_tests successfull";
