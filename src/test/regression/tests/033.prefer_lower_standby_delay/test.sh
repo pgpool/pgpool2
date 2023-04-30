e0d7b6b473ca52e156d944a8f1d3322b35f2d698
Tatsuo Ishii <ishii@sraoss.co.jp>
2023-04-30 15:41:02 +0900

    Fix prefer_lower_delay_standby bug.
    When client connects to pgpool, one of standbys are chosen as the load
    balancing node.  If standby delay exceeds delay_threshold while the
    session continues, prefer_lower_delay_standby will choose the least
    delay standby node as the new load balancing node and set the target
    backend to the node. Unfortunately the decision was made *before* the
    checking that SELECT query includes writing function etc., pgpool
    happily sends SELECT which cannot be executed on standby.  To fix
    this, prefer_lower_delay_standby treatment is moved after the writing
    function etc. check.
    
    033.prefer_lower_standby_delay regression test is modified to include
    the case above. Also I have done some refactoring:
    
    - Remove unnecessary while loop for each clustering mode because the
      test is only useful for streaming replication mode.
    
    - Add checking wal_replay_pause is actually executed.
    
    Bug reported by: https://www.pgpool.net/mantisbt/view.php?id=798
    along with suggested fix.

--- src/test/regression/tests/033.prefer_lower_standby_delay/test.sh
+++ src/test/regression/tests/033.prefer_lower_standby_delay/test.sh
@@ -14,97 +14,153 @@ result=`echo "$major_version >= 10"|bc`
 if [ $result == 1 ];then
 	REPLAY_PAUSE="SELECT pg_wal_replay_pause();"
 	REPLAY_RESUME="SELECT pg_wal_replay_resume();"
+	REPLAY_STATE="SELECT pg_get_wal_replay_pause_state()"
 else
 	REPLAY_PAUSE="SELECT pg_xlog_replay_pause();"
 	REPLAY_RESUME="SELECT pg_xlog_replay_resume();"
+	REPLAY_STATE="SELECT pg_get_xlog_replay_pause_state()"
 fi
 
-for mode in s
-do
-	rm -fr $TESTDIR
-	mkdir $TESTDIR
-	cd $TESTDIR
+# node 1 port number
+PORT1=11003
 
-	# create test environment
-	echo -n "creating test environment..."
-	$PGPOOL_SETUP -m $mode -n 3 || exit 1
-	echo "done."
+# request replication pause and wait for confirmation
+function replay_pause
+{
+    $PSQL -p $PORT1 test -c "$REPLAY_PAUSE"
+    for i in 1 2 3 4
+    do
+	res=`$PSQL -p $PORT1 -q -t test -c "$REPLAY_STATE"|sed 's/ //'g`
+	if [ "$res" = "paused" ];then
+	    break;
+	else
+	    echo pause state: $res
+	fi
+	sleep 1
+    done
+    if [ "$res" != "paused" ];then
+	echo replay pause failed.
+	./shutdownall
+	exit 1
+    fi
+}
 
-	source ./bashrc.ports
-	echo "app_name_redirect_preference_list = 'psql:1'" >> etc/pgpool.conf
-	echo "delay_threshold = 10" >> etc/pgpool.conf
-	echo "prefer_lower_delay_standby = on" >> etc/pgpool.conf
-	echo "sr_check_period = 3" >> etc/pgpool.conf
+rm -fr $TESTDIR
+mkdir $TESTDIR
+cd $TESTDIR
 
-	./startall
+# create test environment
+echo -n "creating test environment..."
+$PGPOOL_SETUP -m s -n 3 || exit 1
+echo "done."
 
-	export PGPORT=$PGPOOL_PORT
+source ./bashrc.ports
+echo "app_name_redirect_preference_list = 'psql:1'" >> etc/pgpool.conf
+echo "delay_threshold = 10" >> etc/pgpool.conf
+echo "prefer_lower_delay_standby = on" >> etc/pgpool.conf
+echo "sr_check_period = 3" >> etc/pgpool.conf
 
-	wait_for_pgpool_startup
+./startall
 
-	$PSQL test <<EOF
+export PGPORT=$PGPOOL_PORT
+
+wait_for_pgpool_startup
+
+$PSQL test <<EOF
 CREATE TABLE t1(i INTEGER);
 CREATE TABLE t2(i INTEGER);
+CREATE SEQUENCE myseq;
 EOF
 
-	echo start: prefer_lower_delay_standby is on.
+echo start: prefer_lower_delay_standby is on.
 
-	$PSQL -p 11003 test -c "$REPLAY_PAUSE"
+# check to see if pgpool selects proper node for load balance
+# at the connection time
 
-	$PSQL test <<EOF
+# pause replay on node 1
+replay_pause
+
+$PSQL test <<EOF
 PGPOOL SET log_min_messages TO DEBUG1;
-INSERT INTO t1 VALUES (1), (2), (3);
+INSERT INTO t1 SELECT * FROM generate_series(1,100);
 SELECT pg_sleep(4);
-SELECT * FROM t1;
+SELECT * FROM t1 LIMIT 1;
 EOF
 
-	fgrep "SELECT * FROM t1;" log/pgpool.log |grep "DB node id: 2">/dev/null 2>&1
-	if [ $? != 0 ];then
-	# expected result not found
-		echo fail: query is sent to primary node.
-		./shutdownall
-		exit 1
-	fi
+fgrep "SELECT * FROM t1 LIMIT 1;" log/pgpool.log |grep "DB node id: 2">/dev/null 2>&1
+if [ $? != 0 ];then
+    # expected result not found
+    echo fail: query is sent to primary node.
+    ./shutdownall
+    exit 1
+fi
 
-	echo ok: query is sent to another standby node.
+echo ok: query is sent to another standby node.
 
-	$PSQL -p 11003 test -c "$REPLAY_RESUME"
+echo resume streaming replication node 1
+$PSQL -p $PORT1 test -c "$REPLAY_RESUME"
+sleep 4
 
-	echo start: prefer_lower_delay_standby is off.
+# check to see if pgpool selects proper node for load balance
+# while in a session. For the test we use SELECT using write
+# function. It should be sent to primary node.
+# see bug #798.
+# https://www.pgpool.net/mantisbt/view.php?id=798
 
-	echo "prefer_lower_delay_standby = off" >> etc/pgpool.conf
-
-	$PGPOOL_INSTALL_DIR/bin/pcp_reload_config -w -h localhost -p $PCP_PORT
-
-	while :
-	do
-		$PSQL test -c "PGPOOL SHOW prefer_lower_delay_standby" |grep off
-		if [ $? = 0 ]; then
-			break
-		fi
-		sleep 1
-	done
-
-	$PSQL -p 11003 test -c "$REPLAY_PAUSE"
-
-	$PSQL test <<EOF
+$PSQL test <<EOF
 PGPOOL SET log_min_messages TO DEBUG1;
-INSERT INTO t2 VALUES (1), (2), (3);
+\! $PSQL -p $PORT1 test -c "$REPLAY_PAUSE"
 SELECT pg_sleep(4);
-SELECT * FROM t2;
+INSERT INTO t1 SELECT * FROM generate_series(1,100);
+SELECT pg_sleep(4);
+SELECT nextval('myseq');
 EOF
 
-	fgrep "SELECT * FROM t2;" log/pgpool.log |grep "DB node id: 0">/dev/null 2>&1
-	if [ $? != 0 ];then
-	# expected result not found
-		echo fail: query is sent to standby node.
-		./shutdownall
-		exit 1
-	fi
+fgrep "SELECT nextval('myseq');" log/pgpool.log |grep "DB node id: 0">/dev/null 2>&1
+if [ $? != 0 ];then
+    # expected result not found
+    echo fail: write query is not sent to primary node.
+    ./shutdownall
+    exit 1
+fi
 
-	echo ok: prefer lower delay standby works.
+echo start: prefer_lower_delay_standby is off.
 
-	./shutdownall
+$PSQL -p $PORT1 test -c "$REPLAY_RESUME"
 
+echo "prefer_lower_delay_standby = off" >> etc/pgpool.conf
+
+$PGPOOL_INSTALL_DIR/bin/pcp_reload_config -w -h localhost -p $PCP_PORT
+
+while :
+do
+    $PSQL test -c "PGPOOL SHOW prefer_lower_delay_standby" |grep off
+    if [ $? = 0 ]; then
+	break
+    fi
+    sleep 1
 done
+
+# pause replay on node 1
+replay_pause
+
+$PSQL test <<EOF
+PGPOOL SET log_min_messages TO DEBUG1;
+INSERT INTO t2 SELECT * FROM generate_series(1,100);
+SELECT pg_sleep(4);
+SELECT * FROM t2 LIMIT 1;
+EOF
+
+fgrep "SELECT * FROM t2 LIMIT 1;" log/pgpool.log |grep "DB node id: 0">/dev/null 2>&1
+if [ $? != 0 ];then
+    # expected result not found
+    echo fail: query is sent to standby node.
+    ./shutdownall
+    exit 1
+fi
+
+echo ok: prefer lower delay standby works.
+
+./shutdownall
+
 exit 0
