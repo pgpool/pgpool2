37dc683f7f15cbf4502b338937e54b9da0a92efd
Muhammad Usama <m.usama@gmail.com>
2023-07-22 18:54:31 +0500

    Disallowing setting the max_spare_children greater than num_init_children.
    Config post-processing stage now throws an error if num_init_children is
    smaller than max_spare_children.
    Commit also adjusts the number of child processes spawned at startup based on
    the configured process management strategy.
    For the Aggressive strategy, max_spare_children number of processes is spawned;
    for the other two strategies, min_spare_children number of children gets
    created at startup.

--- src/config/pool_config_variables.c
+++ src/config/pool_config_variables.c
@@ -4966,6 +4966,13 @@ config_post_processor(ConfigContext context, int elevel)
 				pool_config->max_spare_children,pool_config->min_spare_children)));
 		return false;
 	}
+	if (pool_config->num_init_children < pool_config->max_spare_children)
+	{
+		ereport(elevel,
+				(errmsg("invalid configuration, max_spare_children:%d can't be greater than num_init_children:%d",
+				pool_config->max_spare_children,pool_config->num_init_children)));
+		return false;
+	}
 	return true;
 }
 
