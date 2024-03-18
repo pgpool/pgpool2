9d6561afa78c3666cac149590799f59211694ea7
Tatsuo Ishii <ishii@sraoss.co.jp>
2024-03-18 10:33:16 +0900

    Guard against inappropriate protocol data.
    If a simple query message arrives before a sequence of extended query
    messages ends (that is, no sync message arrives or some ready for
    query messages corresponding the sync message do not arrive yet),
    pgpool could hang.  This is because the query context in the session
    context for the simple query is overwritten by the query contexts of
    the extended query messages.
    
    This commit implements a guard in SimpleQuery() by checking whether
    extended query protocol messages ended. If they do not end, raise a
    FATAL error. A known example detected by this checking is JDBC
    driver's "autosave=always" option. This means pgpool will not accept
    the option after this commit until the issue (sending a simple
    protocol message before ending extended query message protocol) is
    fixed by the JDBC driver side.
    
    Discussion:
    [pgpool-hackers: 4427] Guard against ill mannered frontend
    https://www.pgpool.net/pipermail/pgpool-hackers/2024-February/004428.html

---
+++ src/test/regression/tests/082.guard_against_bad_protocol/test.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------
+# Test script for pgpool hang due to protocol violation.
+# If frontend sends simple query before extended protocol ended, pgpool hangs.
+# 
+# Discussion:
+# [pgpool-general: 8990] autosave=always jdbc option & it only sends query SAVEPOINT PGJDBC_AUTOSAVE and hangs
+# [pgpool-hackers: 4427] Guard against ill mannered frontend
+#
+source $TESTLIBS
+TESTDIR=testdir
+PSQL=$PGBIN/psql
+PG_CTL=$PGBIN/pg_ctl
+PGPROTO=$PGPOOL_INSTALL_DIR/bin/pgproto
+export PGDATABASE=test
+
+rm -fr $TESTDIR
+mkdir $TESTDIR
+cd $TESTDIR
+
+echo -n "creating test environment..."
+$PGPOOL_SETUP || exit 1
+echo "done."
+echo "backend_weight1=0" >> etc/pgpool.conf
+source ./bashrc.ports
+./startall
+wait_for_pgpool_startup
+
+# Wait for 1 seconds before pgproto ended.
+# Usually 1 seconds should be enough to finish pgproto.
+# If test suceeded, pgpool emits an error message:
+# "FATAL:  simple query "SAVEPOINT PGJDBC_AUTOSAVE" arrived before ending an extended query message"
+# grep command below should catch the message.
+timeout 1 $PGPROTO -d $PGDATABASE -p $PGPOOL_PORT -f ../pgproto.data |& grep 'simple query "SAVEPOINT PGJDBC_AUTOSAVE" arrived '
+
+if [ $? != 0 ];then
+# timeout happened or pgproto returned non 0 status
+    echo "test failed."
+    ./shutdownall
+    exit 1
+fi
+./shutdownall
+exit 0
