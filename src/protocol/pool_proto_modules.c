b929717b26aa33a8175ff50b20f7b0ccd76d1109
Tatsuo Ishii <ishii@sraoss.co.jp>
2024-03-18 10:33:16 +0900

    Guard against inappropriate protocol data.
    If a simple query message arrives before a sequence of extended query
    messages ends (that is, no sync message arrives or some ready for
    query messages corresponding the sync message do not arrive yet),
    pgpool could hang.  This is because the query context in the session
    context for the simple query is overwritten by the query contexts of
    the extended query messages.
    
    This commit implements a guard in SimpleQuery() by checking whether
    extended query protocol messages ended. If they do not end, raise a
    FATAL error. A known example detected by this checking is JDBC
    driver's "autosave=always" option. This means pgpool will not accept
    the option after this commit until the issue (sending a simple
    protocol message before ending extended query message protocol) is
    fixed by the JDBC driver side.
    
    Discussion:
    [pgpool-hackers: 4427] Guard against ill mannered frontend
    https://www.pgpool.net/pipermail/pgpool-hackers/2024-February/004428.html

--- src/protocol/pool_proto_modules.c
+++ src/protocol/pool_proto_modules.c
@@ -220,6 +220,20 @@ SimpleQuery(POOL_CONNECTION * frontend,
 	/* save last query string for logging purpose */
 	strlcpy(query_string_buffer, contents, sizeof(query_string_buffer));
 
+	/*
+	 * Check if extended query protocol message ended.  If not, reject the
+	 * query and raise an error to terminate the session to avoid hanging up.
+	 */
+	if (SL_MODE)
+	{
+		if (pool_is_doing_extended_query_message() ||
+			pool_pending_message_head_message())
+
+			ereport(FATAL,
+					(errmsg("simple query \"%s\" arrived before ending an extended query message",
+							query_string_buffer)));
+	}
+
 	/* show ps status */
 	query_ps_status(contents, backend);
 
