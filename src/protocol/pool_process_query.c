5ca0621c66d91641dec3611c3d0b4f81d5990262
Tatsuo Ishii <ishii@postgresql.org>
2024-09-14 09:54:56 +0900

    Fix bug in replication/snapshot isolation mode.
    When INSERT command is received, pgpool automatically issues table
    LOCK command to the target table but it forgot to send the command to
    other than main nodes. This only happened in extended query mode.
    This commit fixes the bug.
    
    Discussion: GitHub issue #69.
    https://github.com/pgpool/pgpool2/issues/69
    Backpatch-through: v4.1

--- src/protocol/pool_process_query.c
+++ src/protocol/pool_process_query.c
@@ -2785,7 +2785,7 @@ insert_lock(POOL_CONNECTION * frontend, POOL_CONNECTION_POOL * backend, char *qu
 					per_node_statement_log(backend, i, qbuf);
 					if (pool_get_session_context(true) && pool_is_doing_extended_query_message())
 					{
-						do_query(MASTER(backend), qbuf, &result, MAJOR(backend));
+						do_query(CONNECTION(backend, i), qbuf, &result, MAJOR(backend));
 						if (result)
 							free_select_result(result);
 					}
