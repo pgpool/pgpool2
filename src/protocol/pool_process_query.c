4eb55054e9c413c14db151c08fad39f5cc9e1352
Tatsuo Ishii <ishii@postgresql.org>
2024-11-15 17:08:26 +0900

    Fix bug in do_query.
    Upon receiving DataRow packet, it converts the number of fields from
    network byte order to host byte order.  Unfortunately it used htons()
    for this purpose instead of ntohs(). This is simply wrong. Similarly it
    used htonl() instead of htohl() while converting the data length from
    network byte order to host byte order. This is wrong too. But
    fortunately both ntohs()/htons() and ntohl()/htonl() swap the byte and
    they bring the same result (i.e. htonl(data_len) ==
    ntohl(data_len). So actually the bug does not hurt anything.
    However a bug is a bug. This commit fixes them.
    
    Backpatch-through: v4.1

--- src/protocol/pool_process_query.c
+++ src/protocol/pool_process_query.c
@@ -2315,7 +2315,7 @@ do_query(POOL_CONNECTION * backend, char *query, POOL_SELECT_RESULT * *result, i
 					if (p)
 					{
 						memcpy(&shortval, p, sizeof(short));
-						num_fields = htons(shortval);
+						num_fields = ntohs(shortval);
 						p += sizeof(short);
 					}
 					else
@@ -2347,7 +2347,7 @@ do_query(POOL_CONNECTION * backend, char *query, POOL_SELECT_RESULT * *result, i
 						if (major == PROTO_MAJOR_V3)
 						{
 							memcpy(&intval, p, sizeof(int));
-							len = htonl(intval);
+							len = ntohl(intval);
 							p += sizeof(int);
 
 							res->nullflags[num_data] = len;
