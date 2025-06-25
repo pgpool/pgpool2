04e09df17e0b17c62d3f11eb5e5f95315c00fd84
Tatsuo Ishii <ishii@postgresql.org>
2025-06-25 19:32:32 +0900

    Test: fix 038.pcp_commands regression test.
    The result of the test showed local host IP. Although the IP can be
    either IPv4 or IPv6, the test script hadn't considered it. To fix
    this, now test.sh converts IPv4 and IPv6 IP to "localhost".

--- src/test/regression/tests/038.pcp_commands/test.sh
+++ src/test/regression/tests/038.pcp_commands/test.sh
@@ -60,7 +60,7 @@ wait_for_pgpool_startup
 $PSQL -a -h localhost -c "SELECT pg_sleep(1)" test &
 $PSQL -a -h localhost test > result 2>&1 <<EOF
 CREATE EXTENSION pgpool_adm;
-SELECT database, status, client_host, statement FROM pcp_proc_info
+SELECT database, status, case when client_host = '127.0.0.1' or client_host = '::1' then 'localhost' end, statement FROM pcp_proc_info
 (host => '', port => $PCP_PORT, username => '$WHOAMI', password => '$WHOAMI')
 WHERE connected = '1' AND backend_id = '0' AND statement = 'SELECT pg_sleep(1)'
 EOF
