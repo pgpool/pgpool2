4a094611ea49ff6c53512b2a8825d6cf46e18b97
Tatsuo Ishii <ishii@postgresql.org>
2025-11-16 15:51:24 +0900

    Fix segfault with CopyOut.
    When "COPY relname TO STDOUT" is executed in the extended query
    protocol mode, pgpool segfaulted.
    
    When read_kind_from_backend() reads a message from backend, it
    extracts the corresponding entry from the pending message queue when
    processing extended query protocol messages. However, if the head of
    the message queue is an "execute" message, some of incoming message
    types are exceptional because other than CommandComplete message
    (which means the execute message finishes) may come from backend. This
    includes DataRow, ErrorResponse, NoticeMessage. Unfortunately we
    overlooked that 'H' (CopyOutResponse) is in the group too. Thus when
    CopyOutResponse comes from backend, the execute pending message is
    removed. If the next message from frontend is Sync (it's often
    happens), read_kind_from_backend() sets session_context->query_context
    to NULL, and calls pool_unset_query_in_progress(), which accesses
    session_context->query_context and segfaults.
    
    The fix is, to add CopyOutResponse to the exception list. Just in
    case, we also add 'd' (CopyData) and 'c' (CopyDone) to the list. This
    may not be actually necessary since CopyData and CopyDone are
    processced in CopyDataRows() though.
    
    Add regression test case to 126.copy_hang (master and v4.7) or
    076.copy_hang (v4.6 or before).
    
    Author: Tatsuo Ishii <ishii@postgresql.org>
    Reported-by: https://github.com/tetesh
    Reviewed-by: Bo Peng <pengbo@sraoss.co.jp>
    Discussion: https://github.com/pgpool/pgpool2/issues/133
    Backpatch-through: v4.2

--- src/protocol/pool_process_query.c
+++ src/protocol/pool_process_query.c
@@ -3799,9 +3799,10 @@ read_kind_from_backend(POOL_CONNECTION * frontend, POOL_CONNECTION_POOL * backen
 	/*
 	 * If we are in in streaming replication mode and we doing an extended
 	 * query, check the kind we just read.  If it's one of 'D' (data row), 'E'
-	 * (error), or 'N' (notice), and the head of the pending message queue was
-	 * 'execute', the message must not be pulled out so that next Command
-	 * Complete message from backend matches the execute message.
+	 * (error), 'N' (notice), 'H' (CopyOutResponse), 'd' (CopyData) or 'c'
+	 * (CopyDone) and the head of the pending message queue was 'execute', the
+	 * message must not be pulled out so that next Command Complete message
+	 * from backend matches the execute message.
 	 *
 	 * Also if it's 't' (parameter description) and the pulled message was
 	 * 'describe', the message must not be pulled out so that the row
@@ -3810,7 +3811,9 @@ read_kind_from_backend(POOL_CONNECTION * frontend, POOL_CONNECTION_POOL * backen
 	if (SL_MODE && pool_is_doing_extended_query_message() && msg)
 	{
 		if ((msg->type == POOL_EXECUTE &&
-			 (*decided_kind == 'D' || *decided_kind == 'E' || *decided_kind == 'N')) ||
+			 (*decided_kind == 'D' || *decided_kind == 'E' ||
+			  *decided_kind == 'N' || *decided_kind == 'H' ||
+			  *decided_kind == 'd' || *decided_kind == 'c')) ||
 			(msg->type == POOL_DESCRIBE && *decided_kind == 't'))
 		{
 			ereport(DEBUG5,
