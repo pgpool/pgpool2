0a90f885e8aa2da78e28d9054073a738c7094a4a
Tatsuo Ishii <ishii@postgresql.org>
2026-04-27 12:58:11 +0900

    Fix pcp main process to remember child pids upon restarting.
    Upon failove/failback, pgpool main process sends a signal to pcp main
    process to request restarting, and pcp main restarts. Problem is, when
    pcp main restarts, it forgets the list of pids of its child pcp worker
    process. As a result, when pgpool main sends SIGUSR2 to pcp main to
    wake up pcp worker process, it cannot find the pid to send the signal
    to, which causes the infinite loop in pcp worker process.
    
    Below is a reproducer script. pcp_promote_node hangs.
    ------------------------------------------------
    pgpool_setup -n 3 --no-stop
    pg_ctl -D data2 stop
    while true
    do
        psql -p 11000 -c "show pool_nodes" test
        if [ $? = 0 ];then
    	break;
        fi
        sleep 1
    done
    psql -p 11000 -c "show pool_nodes" test
    pcp_recovery_node -p 11001 -n 2;pcp_promote_node -p 11001 -n 2 -s -g
    ------------------------------------------------
    
    To fix the problem, we could delay the restarting of pcp main until it
    delivers the signal. Unfortunately this does not work, since pgpool
    main waits for pcp main process to exit. Thus processing failover does
    not proceed in pgpool main.
    
    So I decided to add a new shared memory area
    (Req_info->pcp_worker_pids) to remember the pcp workers pids across
    pcp main restarting. The array size is MAX_REQUEST_QUEUE_SIZE + some
    room (currently 10). If all the slots in the area is used, a warning
    will emitted. However, this should not happen in normal
    circumstances.
    
    When a child process is forked from pcp main, its pid is added to the
    shared memory array. When child process exits, its pid in the array is
    cleared to 0, representing an empty slot.
    
    Author: Tatsuo Ishii <ishii@postgresql.org>
    Reported-by: Taiki Koshino <koshino@sraoss.co.jp>
    Discussion: https://www.postgresql.org/message-id/20260425.223051.1207744844622514060.ishii%40postgresql.org
    Backpatch-through: v4.3

--- src/include/pool.h
+++ src/include/pool.h
@@ -487,6 +487,13 @@ typedef struct
 	int			count;			/* request node ids count */
 } POOL_REQUEST_NODE;
 
+/*
+ * Maximum number of pcp worker child process.  * Since pcp worker process is
+ * forked whenever failover/failback request is made, it should be equal to
+ * MAX_REQUEST_QUEUE_SIZE + some room. 10 is an arbitrary number.
+*/
+#define	MAX_PCP_WORKER_PIDS		MAX_REQUEST_QUEUE_SIZE + 10
+
 typedef struct
 {
 	POOL_REQUEST_NODE request[MAX_REQUEST_QUEUE_SIZE];
@@ -524,6 +531,12 @@ typedef struct
 	bool		query_cache_invalidate_request; /* true if
 												 * pcp_invalidate_query_cache
 												 * requested */
+
+	/*
+	 * pcp worker child pids. This is inherited to new pcp main process to
+	 * track pcp worker child when new pcp worker child starts.
+	 */
+	pid_t		pcp_worker_pids[MAX_PCP_WORKER_PIDS];
 } POOL_REQUEST_INFO;
 
 /* description of row. corresponding to RowDescription message */
