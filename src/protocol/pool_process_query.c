c1eb10dc1df76483b3b793e012d4c40447267e0a
Tatsuo Ishii <ishii@postgresql.org>
2026-08-02 07:21:03 +0900

    Fix do_query to send sync rather than flush.
    When in extended query mode and no explicit transaction has started
    (i.e. implicit transaction), do_query issues a flush message after
    execute message. But this does not close the implicit transaction and
    "DISCARD ALL" in reset_query_list failed with:
    
     "DISCARD ALL cannot run inside a transaction block"
    
    This could happen when query cache is enabled. After an execute and
    sync are issued, pgpool generates internal query in extended query
    mode using do_query:
    
    SELECT oid FROM pg_catalog.pg_database WHERE datname = 'test'
    
    Since do_query never issued a sync for this query, the implicit
    transaction opened by do_query never closed and raised the error.
    
    To fix the issue, send a sync message, rather than a flush message.
    Sync will close the unamed portal in an implicit transaction. But to
    run an unnamed portal, we need to issue bind message and re-create a
    unnamed portal anyway.
    
    Note: the author created the patch for fixing "idle in transaction"
    left in pg_stat_activity, but I failed to reproduce the
    issue. However, since I found the patch solves the issue above, I
    decided to commit the patch anyway.
    
    Author: Achilleas Mantzios <a.mantzios@cloud.gatewaynet.com>
    Reviewed-by: Tatsuo Ishii <ishii@postgresql.org>
    Discussion: https://www.postgresql.org/message-id/25939319-d77c-4df9-9e65-dfb1637ccd33%40cloud.gatewaynet.com
    Backpatch-through: v4.3

--- src/protocol/pool_process_query.c
+++ src/protocol/pool_process_query.c
@@ -1917,6 +1917,7 @@ do_query(POOL_CONNECTION * backend, char *query, POOL_SELECT_RESULT * *result, i
 	int			num_close_complete;
 	int			state;
 	bool		data_pushed;
+	bool		sent_sync = false;	/* true if we sent a Sync (not Flush) */
 
 	data_pushed = false;
 
@@ -2066,17 +2067,14 @@ do_query(POOL_CONNECTION * backend, char *query, POOL_SELECT_RESULT * *result, i
 		pool_write(backend, prepared_name, pname_len);
 
 		/*
-		 * Send sync or flush message. If we are in an explicit transaction,
-		 * sending "sync" is safe because it will not break unnamed portal.
-		 * Also this is desirable because if no user queries are sent after
-		 * do_query(), COMMIT command could cause statement time out, because
-		 * flush message does not clear the alarm for statement time out which
-		 * has been set when do_query() issues query.
+		 * Send Sync message. If we are in an explicit transaction, sending
+		 * "sync" is safe because it will not break user's unnamed portal.  If
+		 * we are not in an explicit transaction, sending a sync message
+		 * closes an unnamed portal.  But next user's bind message will create
+		 * the unnamed portal anyway.
 		 */
-		if (backend->tstate == 'T')
-			pool_write(backend, "S", 1);	/* send "sync" message */
-		else
-			pool_write(backend, "H", 1);	/* send "flush" message */
+		pool_write(backend, "S", 1);	/* send "sync" message */
+		sent_sync = true;
 		len = htonl(sizeof(len));
 		pool_write_and_flush(backend, &len, sizeof(len));
 	}
@@ -2187,7 +2185,7 @@ do_query(POOL_CONNECTION * backend, char *query, POOL_SELECT_RESULT * *result, i
 					return;
 
 				/* If "sync" message was issued, 'Z' is expected. */
-				if (doing_extended && backend->tstate == 'T')
+				if (doing_extended && sent_sync)
 					state |= COMMAND_COMPLETE_RECEIVED;
 				break;
 
@@ -2199,7 +2197,7 @@ do_query(POOL_CONNECTION * backend, char *query, POOL_SELECT_RESULT * *result, i
 				 * If "sync" message was issued, 'Z' is expected, else we are
 				 * done with 'C'.
 				 */
-				if (!doing_extended || backend->tstate != 'T')
+				if (!doing_extended || !sent_sync)
 					state |= COMMAND_COMPLETE_RECEIVED;
 
 				/*
