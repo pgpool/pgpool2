808cccc699d45061257f10e472ea215a3845a9de
Tatsuo Ishii <ishii@postgresql.org>
2026-09-19 06:17:58 +0900

    Fix compiler error.
    The commit 8c1795792 "Fix premature Sync during extended-query
    pipelines" introduced a compile error in Rocky Linux 8.
    
    protocol/pool_proto_modules.c: In function 'ProcessFrontendResponse':
    protocol/pool_proto_modules.c:3165:4: error: a label can only be part of a statement and a declaration is not a statement
        bool  defer_primary_sync;
    
    Author: Tatsuo Ishii <ishii@postgresql.org>
    Backpatch-through: v4.3

--- src/protocol/pool_proto_modules.c
+++ src/protocol/pool_proto_modules.c
@@ -3046,6 +3046,7 @@ ProcessFrontendResponse(POOL_CONNECTION * frontend,
 			POOL_QUERY_CONTEXT *query_context;
 			char	   *query;
 			Node	   *node;
+			bool		defer_primary_sync;
 
 		case 'X':				/* Terminate */
 			if (contents)
@@ -3103,7 +3104,6 @@ ProcessFrontendResponse(POOL_CONNECTION * frontend,
 			break;
 
 		case 'S':				/* Sync */
-			bool		defer_primary_sync;
 			if (pool_config->log_client_messages)
 				ereport(LOG,
 						(errmsg("Sync message from frontend.")));
