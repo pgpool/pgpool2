2da451f98934e749a3b791664654acb3ace67041
Taiki Koshino <koshino@sraoss.co.jp>
2026-09-18 14:05:51 +0900

    Fix response buffer pointer advancement in do_error_execute_command
    The response body is copied len bytes, but the buffer pointer was
    advanced by sizeof(len). When a zero-length response body was received,
    the pointer advanced by four bytes even though no body was copied,
    causing subsequent saved responses to be corrupted.
    
    Advance the pointer by len in both protocol v2 and v3 paths.
    
    Author: Taiki Koshino <koshino@srasoss.co.jp>
    Discussion: https://www.postgresql.org/message-id/OS9P286MB6486821B656397FBCD72BE5394BF2%40OS9P286MB6486.JPNP286.PROD.OUTLOOK.COM
    Backpatch-through: v4.3

--- src/protocol/pool_process_query.c
+++ src/protocol/pool_process_query.c
@@ -1813,7 +1813,7 @@ do_error_execute_command(POOL_CONNECTION_POOL * backend, int node_id, int major)
 
 				}
 				memcpy(p, string, len);
-				p += sizeof(len);
+				p += len;
 			}
 		}
 		else
@@ -1834,7 +1834,7 @@ do_error_execute_command(POOL_CONNECTION_POOL * backend, int node_id, int major)
 							 errdetail("not enough space in buffer")));
 				}
 				memcpy(p, string, len);
-				p += sizeof(len);
+				p += len;
 			}
 		}
 	} while (kind != 'E');
